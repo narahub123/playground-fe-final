6795e2cf6c6b723ebcc888e96e58bf94e639bc81
nara <parknara0213@gmail.com>
2024-12-19 02:57:46 +0900

    Feat: [추가] props 추가
    Refs: #13

--- src/shared/@common/ui/components/Text/Text.tsx
+++ src/shared/@common/ui/components/Text/Text.tsx
@@ -2,21 +2,28 @@ import styles from "./Text.module.css";
 
 interface TextProps {
   text: string; // 내용
-  type?: "heading" | "expl" | "normal"; // 타입
+  type?: "heading1" | "heading2" | "heading3" | "expl" | "normal"; // 타입
+  style?: string; // 스타일 추가
 }
 
-const Text = ({ text, type = "normal" }: TextProps) => {
-  // 태그 결정
-  const Comp = type === "heading" ? "h3" : "p";
-  // 스타일 결정
-  const style =
-    type === "heading"
-      ? styles.heading
-      : type === "expl"
-      ? styles.expl
-      : styles.normal;
+const Text = ({ text, type = "normal", style }: TextProps) => {
+  //빈 텍스트 처리
+  if (!text.trim()) return null;
 
-  return <Comp className={`${style}`}>{text}</Comp>;
+  // 태그 결정
+  const Comp =
+    type === "heading1"
+      ? "h1"
+      : type === "heading2"
+      ? "h2"
+      : type === "heading3"
+      ? "h3"
+      : "p";
+
+  // 스타일 결정
+  const className = styles[type] || styles.normal;
+
+  return <Comp className={`${className} ${style || ""}`}>{text}</Comp>;
 };
 
 export default Text;
