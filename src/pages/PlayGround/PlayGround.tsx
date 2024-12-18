4c785ad90af70933f48bdbb831aecbf3f5657b3e
nara <parknara0213@gmail.com>
2024-12-19 01:53:53 +0900

    Feat: [추가] 로그인 여부에 따른 페이지 구분
    Refs: #5

--- src/pages/PlayGround/PlayGround.tsx
+++ src/pages/PlayGround/PlayGround.tsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
 import styles from "./PlayGround.module.css";
+import { getLogin } from "@features/auth-setting/models/selectors";
+import { PagesLayout } from "@shared/pages/layouts";
+import AuthLayout from "@shared/auth/layouts/AuthLayout/AuthLayout";
 
 const PlayGround = () => {
-  return <div className={styles.playground}>PlayGround</div>;
+  // 로그인 여부
+  const login = useSelector(getLogin);
+
+  return (
+    <div className={styles.playground}>
+      {login ? <PagesLayout /> : <AuthLayout />}
+    </div>
+  );
 };
 
 export default PlayGround;
