2ee0e6a58743da6e370ed68958dc1b352c8aba30
nara <parknara0213@gmail.com>
2024-12-19 11:12:30 +0900

    Feat: [추가] 배경 테마 적용하기
    Ref: #16, #5

--- src/pages/PlayGround/PlayGround.tsx
+++ src/pages/PlayGround/PlayGround.tsx
@@ -3,10 +3,20 @@ import styles from "./PlayGround.module.css";
 import { getLogin } from "@features/auth-setting/models/selectors";
 import { PagesLayout } from "@shared/pages/layouts";
 import AuthLayout from "@shared/auth/layouts/AuthLayout/AuthLayout";
+import { getBgTheme } from "@shared/@common/models/selectors";
+import { useEffect } from "react";
 
 const PlayGround = () => {
   // 로그인 여부
   const login = useSelector(getLogin);
+
+  // 배경 테마
+  const bgTheme = useSelector(getBgTheme);
+
+  // 배경 테마 적용하기
+  useEffect(() => {
+    document.documentElement.dataset.bgTheme = bgTheme;
+  }, [bgTheme]);
 
   return (
     <div className={styles.playground}>
