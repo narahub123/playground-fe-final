0335f9e33324bc7104a5c3bb8969cc35f1511963
nara <parknara0213@gmail.com>
2024-12-22 20:11:48 +0900

    Feat: [추가] TestModal 레아아웃
    Refs: #27

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,131 +1,28 @@
 import styles from "./AuthPage.module.css";
-import { ReactNode } from "react";
-import { Input, Modal, Text } from "@shared/@common/ui/components";
+import { Text } from "@shared/@common/ui/components";
 import { AuthButton } from "@shared/auth/ui/components";
 import {
   useDisclosure,
   useLanguageContent,
 } from "@shared/@common/models/hooks";
 import { AuthButtonItemType } from "@shared/auth/types";
-import { useSelector } from "react-redux";
-import { getUsernameInSignin } from "@features/auth-setting/models/selectors";
-import { setUsernameInSignIn } from "@features/auth-setting/models/slices/reducers";
-import { PASSWORD_MAX, PASSWORD_REGEX } from "@shared/@common/constants";
-import {
-  PASSWORD_REGEX_FORMAT,
-  PASSWORD_REGEX_LENGTH,
-} from "@shared/@common/constants/regExps";
-import { InputErrorType } from "@shared/@common/types";
-
-const list = [
-  { text: "텍스트", value: "값" },
-  { text: "예2", value: "test" },
-];
+import TestModal from "@test/ui/components/TestModal/TestModal";
 
 const AuthPage = () => {
   // 언어 설정
   const { title, heading1, signinList, heading2, loginList } =
     useLanguageContent(["pages", "AuthPage"]);
 
-  const value = useSelector(getUsernameInSignin);
-
-  const { isOpen, onOpen, onClose, curPage, setcurPage } = useDisclosure();
-
-  const error: InputErrorType = {
-    regExp: PASSWORD_REGEX,
-    defaultErrorMsg:
-      "비밀번호는 영문 대문자, 소문자, 숫자, 특수문자가 각각 하나 이상씩 필요합니다.",
-    errorList: [
-      {
-        regExp: PASSWORD_REGEX_FORMAT,
-        errorMsg:
-          "비밀번호는 영문 대문자, 소문자, 숫자, 특수문자만 허용됩니다.",
-      },
-      {
-        regExp: PASSWORD_REGEX_LENGTH,
-        errorMsg: "비밀번호는 8자 이상 30자 이내여야 합니다.",
-      },
-    ],
-    empty: "비밀번호를 입력해주세요.",
-  };
-
-  const pageList: ReactNode[] = [
-    <Modal.Body>
-      {/* <Input
-        field={"password"}
-        fieldName={"유저 아이디"}
-        maxLength={PASSWORD_MAX}
-        inputValue={value}
-        setInputValue={setUsernameInSignIn}
-        // error={error}
-        mode="search"
-        list={list}
-        moveFocusToDropdown={true}
-      />
-      <Input
-        field={"password"}
-        fieldName={"유저 아이디"}
-        maxLength={PASSWORD_MAX}
-        inputValue={value}
-        setInputValue={setUsernameInSignIn}
-        // error={error}
-        mode="search"
-        list={list}
-        moveFocusToDropdown={true}
-      />
-      <Input
-        field={"password"}
-        fieldName={"유저 아이디"}
-        maxLength={PASSWORD_MAX}
-        inputValue={value}
-        setInputValue={setUsernameInSignIn}
-        // error={error}
-        mode="search"
-        list={list}
-        moveFocusToDropdown={true}
-      />
-      <Input
-        field={"password"}
-        fieldName={"유저 아이디"}
-        maxLength={PASSWORD_MAX}
-        inputValue={value}
-        setInputValue={setUsernameInSignIn}
-        // error={error}
-        mode="search"
-        list={list}
-        moveFocusToDropdown={true}
-      /> */}
-      <input type="text" />
-    </Modal.Body>,
-    <Modal.Content>
-      <Modal.Body>바디2</Modal.Body>
-      <Modal.Footer>푸터2</Modal.Footer>
-    </Modal.Content>,
-  ];
+  const { isOpen, onOpen, onClose, curPage, setCurPage } = useDisclosure();
 
   return (
     <div className={styles[`auth-page`]}>
-      <Modal
+      <TestModal
         isOpen={isOpen}
         onClose={onClose}
-        lengthOfList={pageList.length}
         curPage={curPage}
-        setCurPage={setcurPage}
-        width={70}
-        unit="%"
-      >
-        <Modal.Overlay />
-        <Modal.Container>
-          <Modal.CloseButton />
-          <Modal.Content>
-            <Modal.Header>
-              <Modal.Indicator />
-            </Modal.Header>
-            {pageList[curPage]}
-            <Modal.Footer>버튼</Modal.Footer>
-          </Modal.Content>
-        </Modal.Container>
-      </Modal>
+        setCurPage={setCurPage}
+      />
       <header className={styles.header}>
         <Text text={title} />
       </header>
---
+++ src/test/ui/components/TestModal/TestModal.tsx
@@ -0,0 +1,95 @@
+import { getUsernameInSignin } from "@features/auth-setting/models/selectors";
+import { Input, Modal } from "@shared/@common/ui/components";
+import { ReactNode } from "react";
+import { useSelector } from "react-redux";
+import { PASSWORD_MAX, PASSWORD_REGEX } from "@shared/@common/constants";
+import { setUsernameInSignIn } from "@features/auth-setting/models/slices/signinSlice";
+import { InputErrorType } from "@shared/@common/types";
+import {
+  PASSWORD_REGEX_FORMAT,
+  PASSWORD_REGEX_LENGTH,
+} from "@shared/@common/constants/regExps";
+
+interface TestModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  curPage: number;
+  setCurPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const error: InputErrorType = {
+  regExp: PASSWORD_REGEX,
+  defaultErrorMsg:
+    "비밀번호는 영문 대문자, 소문자, 숫자, 특수문자가 각각 하나 이상씩 필요합니다.",
+  errorList: [
+    {
+      regExp: PASSWORD_REGEX_FORMAT,
+      errorMsg: "비밀번호는 영문 대문자, 소문자, 숫자, 특수문자만 허용됩니다.",
+    },
+    {
+      regExp: PASSWORD_REGEX_LENGTH,
+      errorMsg: "비밀번호는 8자 이상 30자 이내여야 합니다.",
+    },
+  ],
+  empty: "비밀번호를 입력해주세요.",
+};
+
+const list = [
+  { text: "텍스트", value: "값" },
+  { text: "예2", value: "test" },
+];
+
+const TestModal = ({
+  isOpen,
+  onClose,
+  curPage,
+  setCurPage,
+}: TestModalProps) => {
+  const value = useSelector(getUsernameInSignin);
+  const pageList: ReactNode[] = [
+    <Modal.Body>
+      <Input
+        field={"password"}
+        fieldName={"유저 아이디"}
+        maxLength={PASSWORD_MAX}
+        inputValue={value}
+        setInputValue={setUsernameInSignIn}
+        error={error}
+        mode="search"
+        list={list}
+        moveFocusToDropdown={true}
+      />
+
+      <input type="text" />
+    </Modal.Body>,
+    <Modal.Content>
+      <Modal.Body>바디2</Modal.Body>
+      <Modal.Footer>푸터2</Modal.Footer>
+    </Modal.Content>,
+  ];
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      lengthOfList={pageList.length}
+      curPage={curPage}
+      setCurPage={setCurPage}
+      width={70}
+      unit="%"
+    >
+      <Modal.Overlay />
+      <Modal.Container>
+        <Modal.CloseButton />
+        <Modal.Content>
+          <Modal.Header>
+            <Modal.Indicator />
+          </Modal.Header>
+          {pageList[curPage]}
+          <Modal.Footer>버튼</Modal.Footer>
+        </Modal.Content>
+      </Modal.Container>
+    </Modal>
+  );
+};
+
+export default TestModal;
