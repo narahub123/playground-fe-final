1423ec68f5edd3b31794a12f6a03fcb7283ed311
nara <parknara0213@gmail.com>
2024-12-29 01:00:58 +0900

    Feat: [추가] InputError 레이아웃
    Refs: #24

--- src/shared/@common/ui/components/Input/ui/InputError/InputError.tsx
+++ src/shared/@common/ui/components/Input/ui/InputError/InputError.tsx
@@ -1,9 +1,21 @@
 import styles from "./InputError.module.css";
+import { useInputContext } from "../../context";
+import Text from "../../../Text/Text";
 
 interface InputErrorProps {}
 
 const InputError = ({}: InputErrorProps) => {
-  return <div className={styles["input__error"]}>InputError</div>;
+  const { errorMessage } = useInputContext();
+  return (
+    <div id="error-message" role="alert" aria-live="polite">
+      <Text
+        text={errorMessage}
+        subClassName={styles["input__error"]}
+        type="expl"
+        status="error"
+      />
+    </div>
+  );
 };
 
 export default InputError;
