98344fcfed51b28f318e8c5d3107bc01fe60a9d9
nara <parknara0213@gmail.com>
2024-12-28 18:50:48 +0900

    Feat: [추가] InputField 값 업데이트
    Refs: #24

--- src/shared/@common/ui/components/Input/ui/InputField/InputField.tsx
+++ src/shared/@common/ui/components/Input/ui/InputField/InputField.tsx
@@ -1,3 +1,4 @@
+import { useAppDispatch } from "@app/store";
 import { useInputContext } from "../../context";
 import styles from "./InputField.module.css";
 import { useEffect } from "react";
@@ -5,8 +6,10 @@ import { useEffect } from "react";
 interface InputFieldProps {}
 
 const InputField = ({}: InputFieldProps) => {
+  const dispatch = useAppDispatch();
   // InputContext를 통해 필요한 값 불러오기
-  const { inputValue, inputRef, setInputRef, field } = useInputContext();
+  const { inputValue, setInputValue, inputRef, setInputRef, field } =
+    useInputContext();
 
   // inputRef 업데이트
   useEffect(() => {
@@ -15,12 +18,19 @@ const InputField = ({}: InputFieldProps) => {
     setInputRef(inputRef);
   }, [inputRef]);
 
+  // onChange 이벤트 핸들러: inputValue 업데이트
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    dispatch(setInputValue(value));
+  };
   return (
     <input
       className={styles["input__field"]}
       value={inputValue} // 기본 값
       ref={inputRef} // input 참조
       id={field} // label과 연결
+      onChange={(e) => handleChange(e)}
     />
   );
 };
