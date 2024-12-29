8c0aabfaca8d32fa94d86509b2d60ccd13931e2c
nara <parknara0213@gmail.com>
2024-12-29 13:33:42 +0900

    Feat: [추가] InputMain 드롭다운 여닫기 구현
    Refs: #24

--- src/shared/@common/ui/components/Input/ui/InputMain/InputMain.tsx
+++ src/shared/@common/ui/components/Input/ui/InputMain/InputMain.tsx
@@ -12,8 +12,15 @@ interface InputMainProps {
 
 const InputMain = ({ children }: InputMainProps) => {
   // useInputContext 훅에서 상태 가져오기
-  const { isFocused, setIsFocused, isValid, inputRef, field } =
-    useInputContext();
+  const {
+    isFocused, // 현재 포커스 상태
+    setIsFocused, // 포커스 상태 업데이트
+    isValid, // 현재 유효성 여부
+    field, // 필드 이름
+    list, // 드롭다운 목록
+    isDropdownOpen, // 현재 드롭다운 여닫기 상태
+    setIsDropdownOpen, // 드롭다운 여닫기 업데이트
+  } = useInputContext();
 
   // InputError와 InputDropdown이 InputMain의 자식 요소로 오지 못하게 제한
   const validChildren = validateChildren(children, [InputError, InputDropdown]);
@@ -35,16 +42,31 @@ const InputMain = ({ children }: InputMainProps) => {
       onMouseDown={() => {
         console.log("클릭");
         setIsFocused(true);
+        // list가 존재하는 경우
+        if (list) {
+          // 드롭다운 여닫기
+          setIsDropdownOpen(!isDropdownOpen);
+        }
       }}
       // 포커스
       onFocus={() => {
         console.log("포커스");
         setIsFocused(true);
+        // list가 존재하는 경우
+        if (list) {
+          // 드롭다운 열기
+          setIsDropdownOpen(true);
+        }
       }}
       // 블러
       onBlur={() => {
         console.log("블러");
         setIsFocused(false);
+        // list가 존재하는 경우
+        if (list) {
+          // 드롭다운 닫기
+          setIsDropdownOpen(false);
+        }
       }}
     >
       <div className={styles[`input__container`]}>{validChildren}</div>
