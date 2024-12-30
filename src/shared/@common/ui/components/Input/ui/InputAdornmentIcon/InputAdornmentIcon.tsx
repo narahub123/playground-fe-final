49ce3c08f9c29a2f2f4ac70b44a378c30c8b99f7
nara <parknara0213@gmail.com>
2024-12-30 23:20:59 +0900

    Feat: [추가] Icons 내에서 아이콘을 선택하는 방법 추가
    Refs: #24

--- src/shared/@common/ui/components/Input/ui/InputAdornmentIcon/InputAdornmentIcon.tsx
+++ src/shared/@common/ui/components/Input/ui/InputAdornmentIcon/InputAdornmentIcon.tsx
@@ -1,34 +1,42 @@
-import { joinClassNames } from "@shared/@common/utils";
-import Icon from "../../../Icon/Icon";
-import { useInputContext } from "../../context";
 import styles from "./InputAdornmentIcon.module.css";
+import { joinClassNames } from "@shared/@common/utils";
+import { Icon } from "@shared/@common/ui/components";
+import { useInputContext } from "@shared/@common/ui/components/Input/context";
+import Icons from "@shared/@common/ui/icons";
 
-interface InputAdornmentIconProps {}
+interface InputAdornmentIconProps {
+  icon?: keyof typeof Icons; // 아이콘 이름
+  subClassName?: string; // 아이콘 스타일 추가
+}
 
-const InputAdornmentIcon = ({}: InputAdornmentIconProps) => {
+const InputAdornmentIcon = ({
+  icon, // 아이콘 이름
+  subClassName, // styles[이름]으로 아이콘 스타일 추가
+}: InputAdornmentIconProps) => {
   const { field, isValid } = useInputContext();
 
   // 필드에 따른 아이콘 자동 설정
-  const iconName =
-    field === "search" // 검색
-      ? "search"
-      : field === "userId" // 유저 아아디
-      ? isValid
-        ? "valid"
-        : "invalid"
-      : undefined; // 필드에 설정된 아이콘이 없는 경우
+  const iconName = icon // icon이 있는 경우
+    ? icon
+    : field === "search" // 검색
+    ? "search"
+    : field === "userId" // 유저 아아디
+    ? isValid
+      ? "valid"
+      : "invalid"
+    : undefined; // 필드에 설정된 아이콘이 없는 경우
 
-  const subClassName =
+  const style = joinClassNames([
     field === "userId" // 유저 아이디
       ? isValid // 유효성 상태
         ? styles[`input__adornment--valid`]
         : styles[`input__adornment--invalid`]
-      : "";
+      : "",
+    subClassName,
+  ]);
 
   if (!iconName) return null;
-  return (
-    <Icon iconName={iconName} subClassName={joinClassNames([subClassName])} />
-  );
+  return <Icon iconName={iconName} subClassName={style} />;
 };
 
 export default InputAdornmentIcon;
