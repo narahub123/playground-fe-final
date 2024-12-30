0192c278fbcacd1d3813677d2b34d8c5757834a3
nara <parknara0213@gmail.com>
2024-12-30 17:30:23 +0900

    Feat: [추가] useCompiledInputError jsdoc 추가
    Refs: #24

--- src/shared/@common/ui/components/Input/hooks/useCompiledInputError.tsx
+++ src/shared/@common/ui/components/Input/hooks/useCompiledInputError.tsx
@@ -1,8 +1,12 @@
 import { useMemo } from "react";
-import { useInputContext } from "../context";
+import { CompiledInputErrorType, useInputContext } from "../context";
 
-// 문자열을 정규 표현식으로 변경
-const useCompiledInputError = () => {
+/**
+ * `useCompiledInputError` 훅은 입력 필드의 에러 정보를 정규 표현식과 메시지로 변환하여 제공합니다.
+ *
+ * @returns {CompiledInputError} 에러 정보를 담은 객체
+ */
+const useCompiledInputError = (): CompiledInputErrorType => {
   const { error } = useInputContext();
   const { regExp, defaultErrorMsg, errorList, empty } = error;
 
@@ -15,13 +19,16 @@ const useCompiledInputError = () => {
       empty,
     };
 
+  /** 기본 에러 정규 표현식을 생성 */
   const defaultErrorRegex = useMemo(() => new RegExp(regExp), [regExp]);
 
+  /** 에러 목록의 각 정규 표현식을 생성 */
   const errorRegexList = useMemo(
     () => (errorList || []).map((err) => new RegExp(err.regExp)),
     [errorList]
   );
 
+  /** 에러 목록의 메시지 배열을 생성 */
   const errorMsgList = useMemo(
     () => (errorList || []).map((err) => err.errorMsg),
     [errorList]
