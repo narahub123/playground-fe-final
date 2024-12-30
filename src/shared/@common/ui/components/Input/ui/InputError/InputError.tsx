8407e2c825fae527b47aef87d161016578e345e0
nara <parknara0213@gmail.com>
2024-12-30 23:54:34 +0900

    Feat: [추가] InputError JsDoc 추가
    Refs: #24

--- src/shared/@common/ui/components/Input/ui/InputError/InputError.tsx
+++ src/shared/@common/ui/components/Input/ui/InputError/InputError.tsx
@@ -1,18 +1,32 @@
 import styles from "./InputError.module.css";
-import { useInputContext } from "../../context";
-import Text from "../../../Text/Text";
+import { useInputContext } from "@shared/@common/ui/components/Input/context";
+import { Text } from "@shared/@common/ui/components";
 
-interface InputErrorProps {}
-
-const InputError = ({}: InputErrorProps) => {
+/**
+ * `InputError` 컴포넌트
+ * - 현재 Input 컴포넌트의 에러 메시지를 표시합니다.
+ * - 접근성을 고려하여 `aria-live`와 `role="alert"` 속성을 사용합니다.
+ *
+ * @returns {JSX.Element | null} 에러 메시지를 렌더링하는 JSX 요소. 에러 메시지가 없으면 `null` 반환.
+ */
+const InputError = () => {
+  // 컨텍스트에서 에러 메시지 가져오기
   const { errorMessage } = useInputContext();
+
+  // 에러 메시지가 없는 경우 렌더링하지 않음
+  if (!errorMessage) return null;
+
   return (
-    <div id="error-message" role="alert" aria-live="polite">
+    <div
+      id="error-message"
+      role="alert" // 에러 메시지임을 스크린 리더에 알림
+      aria-live="polite" // 에러 메시지가 업데이트될 때 읽어줌
+    >
       <Text
-        text={errorMessage}
-        subClassName={styles["input__error"]}
-        type="expl"
-        status="error"
+        text={errorMessage} // 에러 메시지 텍스트
+        subClassName={styles["input__error"]} // 에러 스타일 클래스
+        type="expl" // Text 컴포넌트에서 설명 유형으로 표시
+        status="error" // 에러 상태로 표시
       />
     </div>
   );
