617df02971f626374b775afd9e10dc3fc09258ea
nara <parknara0213@gmail.com>
2025-01-01 00:17:39 +0900

    Fix: [수정] InputDropdown 경로 삭제
    Refs: #28

--- src/shared/@common/ui/components/index.ts
+++ src/shared/@common/ui/components/index.ts
@@ -3,7 +3,6 @@ import Modal from "./Modal/Modal";
 import Icon from "./Icon/Icon";
 import Input from "./Input";
 import Dropdown from "./Dropdown/Dropdown";
-import InputDropdown from "./InputDropdown/InputDropdown";
 import Portal from "./Portal/Portal";
 
-export { Text, Modal, Icon, Input, Dropdown, InputDropdown, Portal };
+export { Text, Modal, Icon, Input, Dropdown, Portal };
