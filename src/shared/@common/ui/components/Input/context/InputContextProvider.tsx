8cc6df399bb88b1efe1d959cd47349be1ed815d9
nara <parknara0213@gmail.com>
2024-12-31 13:47:30 +0900

    Feat: [추가] InputContextProvider jsdoc 추가
    Refs: #24

--- src/shared/@common/ui/components/Input/context/InputContextProvider.tsx
+++ src/shared/@common/ui/components/Input/context/InputContextProvider.tsx
@@ -2,6 +2,17 @@ import { ReactNode } from "react";
 import InputContext from "./InputContext";
 import { InputContextType } from "./Input.types";
 
+/**
+ * InputContext를 제공하는 컴포넌트
+ *
+ * 이 컴포넌트는 `InputContext`에 값을 제공하며, 그 값을 하위 컴포넌트들이 사용할 수 있게 합니다.
+ *
+ * @param {Object} props - 컴포넌트의 props
+ * @param {InputContextType} props.value - 제공할 InputContext 값
+ * @param {ReactNode} props.children - InputContext를 사용할 자식 컴포넌트들
+ *
+ * @returns {JSX.Element} InputContext.Provider를 감싸는 컴포넌트
+ */
 const InputContextProvider = ({
   value,
   children,
