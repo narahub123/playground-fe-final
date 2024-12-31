fcc71b6cf344063eaad49a0958607488bbc02d1f
nara <parknara0213@gmail.com>
2024-12-31 10:18:02 +0900

    Feat: [추가] 기본 DOM이 없는 경우 동적으로 DOM 생성하기
    Refs: #29

--- src/shared/@common/ui/components/Portal/Portal.tsx
+++ src/shared/@common/ui/components/Portal/Portal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { createPortal } from "react-dom";
 
 interface PortalProps {
@@ -7,8 +7,28 @@ interface PortalProps {
 }
 
 const Portal = ({ children, id }: PortalProps) => {
-  const targetNode = document.getElementById(id);
-  return targetNode ? createPortal(children, targetNode) : null;
+  // 대상 DOM 요소 가져오기
+  let targetNode = document.getElementById(id);
+
+  // 대상 DOM 요소가 없을 경우 새로 생성
+  if (!targetNode) {
+    targetNode = document.createElement("div");
+    targetNode.id = id; // ID 설정
+    document.body.appendChild(targetNode); // body에 추가
+  }
+
+  // 컴포넌트 언마운트 시 DOM 정리 : 동적으로 생성된 DOM 노드가 필요 없을 때 자동으로 제거
+  // 메모리 누수와 불필요한 DOM 요소의 축적을 방지
+  useEffect(() => {
+    return () => {
+      if (targetNode?.parentNode) {
+        targetNode.parentNode.removeChild(targetNode);
+      }
+    };
+  }, [targetNode]);
+
+  // React Portal 생성
+  return createPortal(children, targetNode);
 };
 
 export default Portal;
