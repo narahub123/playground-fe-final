9eba2f6a7f8c3da2ecb434664b2507b46eaefcf5
nara <parknara0213@gmail.com>
2024-12-31 13:50:47 +0900

    Feat: [추가] InputContext jsdoc 추가
    Refs: #24

--- src/shared/@common/ui/components/Input/context/InputContext.tsx
+++ src/shared/@common/ui/components/Input/context/InputContext.tsx
@@ -1,6 +1,12 @@
 import { createContext } from "react";
 import { InputContextType } from "./Input.types";
 
+/**
+ * Input 관련 상태와 함수들을 제공하는 Context.
+ * 이 컨텍스트는 `InputContextType`을 통해 input 상태와 관련된 값들을 하위 컴포넌트들에게 전달합니다.
+ *
+ * @type {React.Context<InputContextType | null>}
+ */
 const InputContext = createContext<InputContextType | null>(null);
 
 export default InputContext;
