9ea961dcf8038e22686e23b79ee7796fe673d078
nara <parknara0213@gmail.com>
2025-01-01 01:07:10 +0900

    Feat: [추가] jsdoc 추가
    Refs: #30

--- src/shared/@common/models/hooks/useValidateChildren.tsx
+++ src/shared/@common/models/hooks/useValidateChildren.tsx
@@ -2,11 +2,39 @@ import { Children, ComponentType, isValidElement, ReactNode } from "react";
 import useLanguageContent from "./useLanguageContent";
 
 interface UseValidateChildrenProps {
+  /**
+   * 검사할 자식 요소들.
+   */
   children: ReactNode;
-  invalidComponents: ComponentType[]; // 유효하지 않은 컴포넌트 배열
-  showErrorMessage?: boolean; // 에러 메시지 표시 여부 기본값은 true
+
+  /**
+   * 유효하지 않은 컴포넌트 타입 배열.
+   */
+  invalidComponents: ComponentType[];
+
+  /**
+   * 에러 메시지 표시 여부 (기본값: true).
+   */
+  showErrorMessage?: boolean;
 }
 
+/**
+ * 자식 요소들이 유효한지 검사하고, 유효하지 않은 경우 에러 메시지를 출력합니다.
+ *
+ * @param {UseValidateChildrenProps} props - 유효성 검사에 필요한 속성들.
+ * @param {ReactNode} props.children - 검사할 자식 요소들.
+ * @param {ComponentType[]} props.invalidComponents - 유효하지 않은 컴포넌트 타입 배열.
+ * @param {boolean} [props.showErrorMessage=true] - 에러 메시지 표시 여부.
+ * @returns {ReactNode[]} 유효한 자식 요소 또는 에러 메시지를 포함한 배열.
+ *
+ * @example
+ * // 사용 예시
+ * useValidateChildren({
+ *   children: <div><InvalidComponent /></div>,
+ *   invalidComponents: [InvalidComponent],
+ *   showErrorMessage: true,
+ * });
+ */
 const useValidateChildren = ({
   children,
   invalidComponents,
