1e4472582e84933c32aa6e48a8f8008299aef148
nara <parknara0213@gmail.com>
2025-01-01 19:24:49 +0900

    Feat: [추가] ModalHeader 레이아웃
    Refs: #21

--- src/shared/@common/ui/components/Modal/ui/ModalHeader/ModalHeader.tsx
+++ src/shared/@common/ui/components/Modal/ui/ModalHeader/ModalHeader.tsx
@@ -1,9 +1,12 @@
+import { ReactNode } from "react";
 import styles from "./ModalHeader.module.css";
 
-interface ModalHeaderProps {}
+interface ModalHeaderProps {
+  children: ReactNode;
+}
 
-const ModalHeader = ({}: ModalHeaderProps) => {
-  return <div className={styles["modal__header"]}>ModalHeader</div>;
+const ModalHeader = ({ children }: ModalHeaderProps) => {
+  return <div className={styles["modal__header"]}>{children}</div>;
 };
 
 export default ModalHeader;
