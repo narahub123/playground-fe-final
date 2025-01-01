0dcb916c7667948a64340cc2b7add3c74fa71e1b
nara <parknara0213@gmail.com>
2025-01-01 23:29:08 +0900

    Feat: [추가] ModalOverlay 오버레이 클릭시 모달 닫기
    Refs: #21

--- src/shared/@common/ui/components/Modal/ui/ModalOverlay/ModalOverlay.tsx
+++ src/shared/@common/ui/components/Modal/ui/ModalOverlay/ModalOverlay.tsx
@@ -1,3 +1,4 @@
+import { useModalContext } from "../../hooks";
 import styles from "./ModalOverlay.module.css";
 
 /**
@@ -44,12 +45,15 @@ const ModalOverlay = ({
   backgroundColor = "rgb(0, 0, 0)",
   opacity = 0.6,
 }: ModalOverlayProps) => {
+  const { onClose } = useModalContext();
+
   return (
     <div
       className={styles["modal__overlay"]}
       style={{ backgroundColor, opacity }}
       role="presentation" // 스크린 리더가 읽지 않도록 설정
       aria-hidden={true} // 접근성 트리에서 제외
+      onClick={onClose} // overlay를 클릭하면 모달 닫기
     />
   );
 };
