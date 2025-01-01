c35d76b9e09d80a0983d01647da34be599574124
nara <parknara0213@gmail.com>
2025-01-01 19:58:36 +0900

    Feat: [추가] ModalContainer 포커스 트랩 적용
    Refs: #21

--- src/shared/@common/ui/components/Modal/ui/ModalContainer/ModalContainer.tsx
+++ src/shared/@common/ui/components/Modal/ui/ModalContainer/ModalContainer.tsx
@@ -1,17 +1,23 @@
 import styles from "./ModalContainer.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 import { useModalContext } from "@shared/@common/ui/components/Modal/hooks";
+import { useFocusTrap } from "@shared/@common/models/hooks";
 
 interface ModalContainerProps {
   children: ReactNode;
 }
 
 const ModalContainer = ({ children }: ModalContainerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const { width, unit } = useModalContext();
+
+  useFocusTrap({ containerRef, firstFocus: 1 });
+
   return (
     <div
       className={styles["modal__container"]}
       style={{ width: `${width}${unit}` }}
+      ref={containerRef}
     >
       {children}
     </div>
