07a7fef29ab16fa69bc16892e83249d09a97048f
nara <parknara0213@gmail.com>
2025-01-01 23:49:48 +0900

    Feat: [추가] ModalContent jsdoc 추가
    Refs: #21

--- src/shared/@common/ui/components/Modal/ui/ModalContent/ModalContent.tsx
+++ src/shared/@common/ui/components/Modal/ui/ModalContent/ModalContent.tsx
@@ -1,11 +1,34 @@
 import { ReactNode } from "react";
 import styles from "./ModalContent.module.css";
 
+/**
+ * ModalContentProps는 ModalContent 컴포넌트에 전달되는 속성들을 정의합니다.
+ */
 interface ModalContentProps {
+  /**
+   * 모달 콘텐츠 내부에 렌더링될 내용입니다.
+   * @type {ReactNode}
+   */
   children: ReactNode;
 }
 
+/**
+ * 모달의 콘텐츠 영역을 렌더링하는 컴포넌트입니다.
+ * 이 컴포넌트는 모달 내부의 실제 콘텐츠를 포함하는 역할을 합니다.
+ *
+ * @component
+ * @param {Object} props - 컴포넌트에 전달되는 속성들
+ * @param {ReactNode} props.children - 모달 콘텐츠로 렌더링될 자식 요소입니다.
+ *
+ * @returns {JSX.Element} 모달 콘텐츠를 감싸는 `div` 요소입니다.
+ *
+ * @example
+ * <ModalContent>
+ *   <p>모달 내용이 여기에 표시됩니다.</p>
+ * </ModalContent>
+ */
 const ModalContent = ({ children }: ModalContentProps) => {
+  // 모달 콘텐츠를 감싸는 div 요소를 렌더링합니다.
   return <div className={styles["modal__content"]}>{children}</div>;
 };
 
