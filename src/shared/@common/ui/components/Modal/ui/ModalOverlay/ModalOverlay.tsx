4350881794696dcd27721b2536a286040bcef61e
nara <parknara0213@gmail.com>
2025-01-01 23:15:46 +0900

    Feat: [추가] ModalOverlay 배경 커스텀 추가
    Refs: #21

--- src/shared/@common/ui/components/Modal/ui/ModalOverlay/ModalOverlay.tsx
+++ src/shared/@common/ui/components/Modal/ui/ModalOverlay/ModalOverlay.tsx
@@ -1,9 +1,20 @@
 import styles from "./ModalOverlay.module.css";
 
-interface ModalOverlayProps {}
+interface ModalOverlayProps {
+  backgroundColor?: string;
+  opacity?: number;
+}
 
-const ModalOverlay = ({}: ModalOverlayProps) => {
-  return <div className={styles["modal__overlay"]} />;
+const ModalOverlay = ({
+  backgroundColor = "rgb(0, 0, 0)",
+  opacity = 0.6,
+}: ModalOverlayProps) => {
+  return (
+    <div
+      className={styles["modal__overlay"]}
+      style={{ backgroundColor, opacity }}
+    />
+  );
 };
 
 export default ModalOverlay;
