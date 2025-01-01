e05b21a4faa7cadcab7d0b5d27cd90ffba9359ab
nara <parknara0213@gmail.com>
2025-01-01 19:11:07 +0900

    Feat: [추가] ModalOverlay 레이아웃
    Refs: #21

--- src/shared/@common/ui/components/Modal/ui/ModalOverlay/ModalOverlay.tsx
+++ src/shared/@common/ui/components/Modal/ui/ModalOverlay/ModalOverlay.tsx
@@ -3,7 +3,7 @@ import styles from "./ModalOverlay.module.css";
 interface ModalOverlayProps {}
 
 const ModalOverlay = ({}: ModalOverlayProps) => {
-  return <div className={styles["modal__overlay"]}>ModalOverlay</div>;
+  return <div className={styles["modal__overlay"]} />;
 };
 
 export default ModalOverlay;
