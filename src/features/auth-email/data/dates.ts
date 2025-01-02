8ede3d8f77e86eac2d86ba6462a48a22a21ee28d
nara <parknara0213@gmail.com>
2025-01-02 15:40:08 +0900

    Fix: [수정] 날짜 단위에 언어 설정 적용을 위해서 함수형으로 변경
    Refs: #33

--- src/features/auth-email/data/dates.ts
+++ src/features/auth-email/data/dates.ts
@@ -6,41 +6,42 @@ const thisYear = today.getFullYear(); // 년
 const thisMonth = today.getMonth(); // 월
 
 /**
- * 년도 목록을 생성하는 배열
+ * 년도 목록을 생성하는 함수
  *
- * @constant
- * @type {Array<{text: string, value: string}>}
+ * @param {string} unit - 년도를 나타낼 때 추가할 단위 (예: "년")
+ * @returns {Array<{text: string, value: string}>} 년도 목록
  * @description
- * - BASE_YAER부터 현재 연도까지의 목록을 생성하고, 각 년도를 "YYYY년" 형태로 표시합니다.
+ * - BASE_YAER부터 현재 연도까지의 목록을 생성하고, 각 년도를 `YYYY${unit}` 형태로 표시합니다.
  */
-const birthYearList = Array.from({ length: thisYear - BASE_YAER + 1 })
-  .map((_, index) => ({
-    text: (BASE_YAER + index).toString() + "년",
-    value: (BASE_YAER + index).toString(),
-  }))
-  .reverse();
+const birthYearList = (unit: string) =>
+  Array.from({ length: thisYear - BASE_YAER + 1 })
+    .map((_, index) => ({
+      text: (BASE_YAER + index).toString() + unit,
+      value: (BASE_YAER + index).toString(),
+    }))
+    .reverse();
 
 /**
- * 월 목록을 정의한 배열
+ * 월 목록을 생성하는 함수
  *
- * @constant
- * @type {Array<{text: string, value: string}>}
+ * @param {string} unit - 월을 나타낼 때 추가할 단위 (예: "월")
+ * @returns {Array<{text: string, value: string}>} 월 목록
  * @description
- * - 1월부터 12월까지의 목록을 "1월", "2월" ... "12월" 형태로 표시합니다.
+ * - 1월부터 12월까지의 목록을 생성하고, 각 월을 `M${unit}` 형태로 표시합니다.
  */
-const birthMonthList = [
-  { text: "1월", value: "1" },
-  { text: "2월", value: "2" },
-  { text: "3월", value: "3" },
-  { text: "4월", value: "4" },
-  { text: "5월", value: "5" },
-  { text: "6월", value: "6" },
-  { text: "7월", value: "7" },
-  { text: "8월", value: "8" },
-  { text: "9월", value: "9" },
-  { text: "10월", value: "10" },
-  { text: "11월", value: "11" },
-  { text: "12월", value: "12" },
+const birthMonthList = (unit: string) => [
+  { text: `1${unit}`, value: "1" },
+  { text: `2${unit}`, value: "2" },
+  { text: `3${unit}`, value: "3" },
+  { text: `4${unit}`, value: "4" },
+  { text: `5${unit}`, value: "5" },
+  { text: `6${unit}`, value: "6" },
+  { text: `7${unit}`, value: "7" },
+  { text: `8${unit}`, value: "8" },
+  { text: `9${unit}`, value: "9" },
+  { text: `10${unit}`, value: "10" },
+  { text: `11${unit}`, value: "11" },
+  { text: `12${unit}`, value: "12" },
 ];
 
 /**
@@ -48,6 +49,7 @@ const birthMonthList = [
  *
  * @param {string | number} [year=thisYear] - 대상 년도 (기본값: 현재 년도)
  * @param {string | number} [month=thisMonth] - 대상 월 (기본값: 현재 월)
+ * @param {string} unit - 일자를 나타낼 때 추가할 단위 (예: "일")
  * @returns {Array<{text: string, value: string}>} 일 목록
  * @description
  * - 지정된 년도와 월에 맞는 일 목록을 생성합니다.
@@ -56,14 +58,15 @@ const birthMonthList = [
  */
 const birthDateList = (
   year: string | number = thisYear,
-  month: string | number = thisMonth
+  month: string | number = thisMonth,
+  unit: string
 ) => {
   const target = new Date(Number(year), Number(month), 0);
 
   const lastDate = target.getDate();
 
   return Array.from({ length: lastDate }).map((_, index) => ({
-    text: index + 1 + "일",
+    text: index + 1 + unit,
     value: (index + 1).toString(),
   }));
 };
