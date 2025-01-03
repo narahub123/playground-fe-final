51a67ad34f507e181ef08403130d22b526272564
nara <parknara0213@gmail.com>
2025-01-03 16:42:54 +0900

    Feat: [추가] InputLabel inputValue 에러 상태인 경우 색상 변경 추가
    Refs: #24

--- src/shared/@common/ui/components/Input/ui/InputLabel/InputLabel.tsx
+++ src/shared/@common/ui/components/Input/ui/InputLabel/InputLabel.tsx
@@ -16,20 +16,25 @@ const InputLabel = () => {
    * `useInputContext` 훅을 사용하여 현재 입력 필드의 라벨 텍스트와 포커스 상태를 가져옵니다.
    * - `label`: 라벨에 표시될 텍스트
    * - `isFocused`: 입력 필드가 포커스 상태인지 여부
+   * - `isValid`: inputValue의 값이 유효한지 여부
    */
-  const { label, isFocused } = useInputContext();
+  const { label, isFocused, isValid } = useInputContext();
 
   return (
     /**
      * `Text` 컴포넌트를 사용하여 라벨 텍스트를 렌더링합니다.
      * - `text`: 라벨 텍스트
-     * - `subClassName`: 포커스 상태에 따라 스타일을 동적으로 적용
+     * - `subClassName`: 포커스 상태와 inputValue의 유효성 상태에 따라 스타일을 동적으로 적용
      */
     <Text
       text={label}
       subClassName={joinClassNames([
         styles["input__label"],
-        isFocused ? styles["input__label--focused"] : "",
+        isFocused
+          ? isValid
+            ? styles["input__label--focused"]
+            : styles[`input__label--error`]
+          : "",
       ])}
     />
   );
