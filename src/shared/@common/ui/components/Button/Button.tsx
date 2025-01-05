8f511109fd6551db54db6beda36485e5e7a31eb4
nara <parknara0213@gmail.com>
2025-01-05 14:04:19 +0900

    Feat: [추가] Button 레이아웃
    Refs: #35

---
+++ src/shared/@common/ui/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import styles from "./Button.module.css";
+import { joinClassNames } from "@shared/@common/utils";
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick: (value?: any) => void;
+  isValid?: boolean;
+}
+
+const Button = ({ children, onClick, isValid }: ButtonProps) => {
+  /**
+   * 클래스 이름을 결합하여 하나의 문자열로 반환합니다.
+   * @type {string} 결합된 클래스 이름
+   */
+  const classNames = joinClassNames([styles["button"]]);
+
+  return (
+    <button className={classNames} disabled={!isValid} onClick={onClick}>
+      {children || "문자열 혹은 아이콘을 넣어주세요."}
+    </button>
+  );
+};
+
+export default Button;
--- src/shared/@common/ui/components/index.ts
+++ src/shared/@common/ui/components/index.ts
@@ -4,5 +4,6 @@ import Icon from "./Icon/Icon";
 import Input from "./Input";
 import Dropdown from "./Dropdown/Dropdown";
 import Portal from "./Portal/Portal";
+import Button from "./Button/Button";
 
-export { Text, Modal, Icon, Input, Dropdown, Portal };
+export { Text, Modal, Icon, Input, Dropdown, Portal, Button };
