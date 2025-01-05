895339d4567747f0c14dc80243dc94e4f7812452
nara <parknara0213@gmail.com>
2025-01-06 00:25:19 +0900

    Feat: [추가] loading 추가
    Refs: #35

--- src/shared/@common/ui/components/Button/Button.tsx
+++ src/shared/@common/ui/components/Button/Button.tsx
@@ -1,6 +1,8 @@
 import styles from "./Button.module.css";
 import { joinClassNames } from "@shared/@common/utils";
 import { ReactNode } from "react";
+import Spinner from "../Spinner/Spinner";
+import Text from "../Text/Text";
 
 interface ButtonProps {
   children: ReactNode;
@@ -8,6 +10,8 @@ interface ButtonProps {
   variant?: "solid" | "subtle" | "surface" | "outline" | "ghost" | "plain";
   colorPalette?: "default" | "colorTheme";
   isValid?: boolean;
+  loading?: boolean;
+  loadingText?: string;
 }
 
 const Button = ({
@@ -16,6 +20,8 @@ const Button = ({
   isValid = true,
   variant = "solid",
   colorPalette = "default",
+  loading = false,
+  loadingText,
 }: ButtonProps) => {
   /**
    * 클래스 이름을 결합하여 하나의 문자열로 반환합니다.
@@ -29,7 +35,14 @@ const Button = ({
 
   return (
     <button className={classNames} disabled={!isValid} onClick={onClick}>
-      {children || "문자열 혹은 아이콘을 넣어주세요."}
+      {loading ? (
+        <div className={styles[`button__loading`]}>
+          <Spinner loadingText={loadingText} />
+          {loadingText && <Text text={loadingText} />}
+        </div>
+      ) : (
+        children || "문자열 혹은 아이콘을 넣어주세요."
+      )}
     </button>
   );
 };
