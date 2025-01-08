8fa0725b9b3810d128fb4390baf9cbdf4036e91f
nara <parknara0213@gmail.com>
2025-01-08 11:26:04 +0900

    Fix: [수정] 해당 객체가 포함한 모든 키를 정확히 적어줘야 하는 문제 발생 -> 우선 첫번째 키까지만 적어주는 것으로 변경
    Refs: #15

--- src/shared/@common/models/hooks/useLanguageContent.tsx
+++ src/shared/@common/models/hooks/useLanguageContent.tsx
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux";
 import { getLanguage } from "../selectors";
 import { languages } from "@shared/@common/data";
 
-type ContentType = keyof (typeof languages)["ko-KR"];
+type ContentType = (typeof languages)["ko-KR"];
 
 type NestedKeys<T> = T extends object
-  ? { [K in keyof T]: [K, ...NestedKeys<T[K]>] }[keyof T]
+  ? { [K in keyof T]: [K, ...NestedKeys<T[K]>] | [K] }[keyof T]
   : [];
 
-type KeywordsType = NestedKeys<ContentType>;
+type KeywordsType = [...NestedKeys<ContentType>];
 
 const useLanguageContent = (keywords: KeywordsType) => {
   const language = useSelector(getLanguage);
