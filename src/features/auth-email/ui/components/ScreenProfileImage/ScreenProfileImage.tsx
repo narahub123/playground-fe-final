d13942b9f30cfa1fda5a8c02a85cef4b16be8215
nara <parknara0213@gmail.com>
2025-01-13 01:25:25 +0900

    Fix: [수정] ProfileImage 및 유효성 추가
    Refs: #46

--- src/features/auth-email/ui/components/ScreenProfileImage/ScreenProfileImage.tsx
+++ src/features/auth-email/ui/components/ScreenProfileImage/ScreenProfileImage.tsx
@@ -1,17 +1,22 @@
-import { defaultProfileImage } from "@shared/@common/assets";
 import styles from "./ScreenProfileImage.module.css";
-import { useLanguageContent } from "@shared/@common/models/hooks";
-import { Button, Lightbox, Modal, Text } from "@shared/@common/ui/components";
+import {
+  useLanguageContent,
+  useValidationChecker,
+} from "@shared/@common/models/hooks";
+import {
+  Button,
+  Modal,
+  ProfileImage,
+  Text,
+} from "@shared/@common/ui/components";
 import {
   useModalContext,
   useModalPagination,
 } from "@shared/@common/ui/components/Modal/hooks";
 import { joinClassNames } from "@shared/@common/utils";
-import {
-  useLightboxDisclosure,
-  useLightboxPagination,
-  useLightboxPostDisclosure,
-} from "@shared/@common/ui/components/Lightbox/hooks";
+import { useSelector } from "react-redux";
+import { getUserInSignup } from "@features/auth-setting/models/selectors";
+import { useEffect } from "react";
 
 interface ScreenProfileImageProps {
   className?: string;
@@ -19,8 +24,20 @@ interface ScreenProfileImageProps {
 }
 
 const ScreenProfileImage = ({ className }: ScreenProfileImageProps) => {
+  const user = useSelector(getUserInSignup);
   const { setScreenValidations } = useModalContext();
   const { moveNext } = useModalPagination();
+
+  const { setIsValid, validationResult } = useValidationChecker({
+    fields: ["profileImage"],
+    sliceState: user,
+    setScreenValidations,
+    screenName: "ScreenProfileImage",
+  });
+
+  useEffect(() => {
+    setIsValid(true);
+  }, []);
 
   // 언어 설정
   const { button, title, expl } = useLanguageContent([
@@ -40,10 +57,14 @@ const ScreenProfileImage = ({ className }: ScreenProfileImageProps) => {
           <Text text={title} type="heading2" />
           <Text text={expl} type="expl" />
         </div>
-        djdkj
+        <ProfileImage width="10rem" rounded="full" isSignup />
       </Modal.Body>
       <Modal.Footer>
-        <Button colorPalette="colorTheme" onClick={moveNext}>
+        <Button
+          colorPalette="colorTheme"
+          onClick={moveNext}
+          isValid={validationResult}
+        >
           {button.ignore}
         </Button>
       </Modal.Footer>
