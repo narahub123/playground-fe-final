d0b41730e0e7e8d3e6816885c190edbc386561c1
nara <parknara0213@gmail.com>
2025-01-13 15:01:48 +0900

    Fix: [수정] useValidationChecker 변수에서 screenName 이름 수정
    Refs: #43

--- src/features/auth-email/ui/components/ScreenUserId/ScreenUserId.tsx
+++ src/features/auth-email/ui/components/ScreenUserId/ScreenUserId.tsx
@@ -40,7 +40,7 @@ const ScreenUserId = ({ className }: ScreenUserIdProps) => {
     fields: ["userId"],
     sliceState: user,
     setScreenValidations,
-    screenName: "ScreenPersonalInfo",
+    screenName: "ScreenUserId",
   });
 
   /**
