ea44a5625278a362c43e30a20ac41b97854275ac
nara <parknara0213@gmail.com>
2025-02-03 10:22:41 +0900

    Feat: [추가] 선택항목으로 스크롤 이동
    Refs: #88

--- src/shared/@common/ui/components/Select/ui/SelectOption/SelectOption.tsx
+++ src/shared/@common/ui/components/Select/ui/SelectOption/SelectOption.tsx
@@ -1,22 +1,36 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import styles from "./SelectOption.module.css";
 import { joinClassNames } from "@shared/@common/utils";
 
 interface SelectOptionProps {
   children: ReactNode;
+  value: string;
   onMouseDown: (value: any) => void;
   className?: string;
 }
 
 const SelectOption = ({
   children,
+  value,
   onMouseDown,
   className,
 }: SelectOptionProps) => {
+  const optionRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    if (!optionRef.current) return;
+
+    const option = optionRef.current;
+
+    if (option.className.includes("selected")) {
+      option.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, [value]);
+
   const classNames = joinClassNames([styles["select__option"], className]);
 
   return (
-    <li className={classNames} onMouseDown={onMouseDown}>
+    <li className={classNames} onMouseDown={onMouseDown} ref={optionRef}>
       {children}
     </li>
   );
