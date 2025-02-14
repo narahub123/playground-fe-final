93f679043303f792f21542a1c84e4a0ba3f83c0a
nara <parknara0213@gmail.com>
2025-02-15 02:23:49 +0900

    Fix: [수정] try-catch문 삭제 api 응답 값을 그대로 전달
    Refs: #45

--- src/shared/auth/apis/signup.ts
+++ src/shared/auth/apis/signup.ts
@@ -26,45 +26,19 @@ const checkEmailDuplicateInSignupAPI = async (email: string) => {
  * @throws {Error} - 네트워크 오류 또는 응답 오류 발생 시 에러를 던집니다.
  */
 const checkUserIdDuplicateInSignupAPI = async (userId: string) => {
-  try {
-    // API 호출
-    const response = await fetch(`${BASE_URL}/users/check-duplication/userid`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userId }),
-    });
+  // API 호출
+  const response = await fetch(`${BASE_URL}/users/check-duplication/userid`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userId }),
+  });
 
-    // 응답이 성공적이지 않으면 에러 발생
-    if (!response.ok) {
-      throw new Error("사용자 아이디 중복 확인 실패");
-    }
+  // 응답 JSON 파싱
+  const result = await response.json();
 
-    // 응답 JSON 파싱
-    const result = await response.json();
-
-    console.log(result);
-
-    return { isDuplicate: result.data.isDuplicate };
-  } catch (err: any) {
-    // 네트워크 오류 및 서버 연결 오류 처리
-    if (err instanceof TypeError) {
-      console.error(
-        "서버와 연결되지 않았습니다. 네트워크 문제를 확인해주세요."
-      );
-    } else if (err.message === "Failed to fetch") {
-      console.error(
-        "서버에 접근할 수 없습니다. 서버가 실행 중인지 확인하세요."
-      );
-    } else {
-      // 기타 알 수 없는 오류 처리
-      console.error("알 수 없는 오류 발생:", err.message);
-    }
-
-    // 중복 체크 실패 시 "disconnect" 타입 반환
-    return { isDuplicate: true, type: "disconnect" };
-  }
+  return result;
 };
 
 const registerUserAPI = async (user: SignupState) => {
