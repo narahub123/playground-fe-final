72215e1527d05fdf7bbad317c42cfca139cfcf1b
nara <parknara0213@gmail.com>
2025-02-16 15:17:35 +0900

    Fix: [수정] - 매개변수 삭제 - ip 정보 반환
    Refs: #56

--- src/shared/@common/models/hooks/useIpInfo.tsx
+++ src/shared/@common/models/hooks/useIpInfo.tsx
@@ -1,26 +1,16 @@
-import { useEffect } from "react";
-import { useAppDispatch } from "@app/store";
+import { useEffect, useState } from "react";
 import { fetchIPAPI } from "@shared/@common/apis";
 
-const useIpInfo = (
-  reducer: (value?: any) => { type: string; payload: any }
-) => {
-  const dispatch = useAppDispatch();
+const useIpInfo = () => {
+  const [ip, setIp] = useState("");
 
-  // 비동기 함수 내부에서 호출
-  const getIpInfo = async () => {
-    try {
-      const ip = await fetchIPAPI();
-      dispatch(reducer(ip)); // IP 정보 dispatch
-    } catch (error) {
-      console.error("IP 정보 가져오기 실패", error);
-    }
-  };
-
-  // 모달 열리거나 특정 시점에 호출할 때 getIpInfo() 호출
   useEffect(() => {
-    getIpInfo();
-  }, [dispatch]); // `dispatch`는 React의 상태나 액션에 의존하므로 의존성 배열에 추가
+    fetchIPAPI()
+      .then(setIp) // 성공하면 바로 상태 업데이트
+      .catch((error) => console.error("IP 정보 가져오기 실패", error));
+  }, []);
+
+  return ip;
 };
 
 export default useIpInfo;
