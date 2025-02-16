7ecab79e07c63264bd432d3af85e5363cf68b0e4
nara <parknara0213@gmail.com>
2025-02-16 15:12:38 +0900

    Fix: [수정] - 매개변수 삭제 - os, browser 감지 로직 개선 - return 값 추가
    Refs: #55

--- src/shared/@common/models/hooks/useDeviceInfo.tsx
+++ src/shared/@common/models/hooks/useDeviceInfo.tsx
@@ -1,60 +1,49 @@
-import { useAppDispatch } from "@app/store";
 import { DeviceInfoType } from "@shared/@common/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const useDeviceInfo = (
-  reducer: (value?: any) => { type: string; payload: any }
-) => {
-  const dispatch = useAppDispatch();
+const useDeviceInfo = () => {
+  const [device, setDevice] = useState<DeviceInfoType>({
+    type: "Web",
+    os: "Unknown",
+    browser: "Unknown",
+  });
 
   useEffect(() => {
+    if (typeof window === "undefined") return; // SSR 환경 방어 코드
     const userAgent = window.navigator.userAgent.toLowerCase();
 
-    let device: DeviceInfoType = {
-      type: "Web",
-      os: "Unknown",
-      browser: "Unknown",
-    };
-
-    // 기기 구분
-    device.type = /mobi|android|iphone|ipod/i.test(userAgent)
-      ? "Mobile"
-      : /ipad|tablet/i.test(userAgent)
-      ? "Tablet"
-      : "Web";
-
-    // 운영 체제 구분
-    device.os = /windows/.test(userAgent)
-      ? "Windows"
-      : /mac os/.test(userAgent) || /macintosh/.test(userAgent)
-      ? "MacOS"
-      : /linux/.test(userAgent)
-      ? "Linux"
-      : /android/.test(userAgent)
-      ? "Android"
-      : /iphone|ipad|ipod/.test(userAgent)
-      ? "iOS"
-      : "Unknown";
-
-    // 브라우저 구분
-    device.browser =
-      /chrome/.test(userAgent) &&
-      !/edge/.test(userAgent) &&
-      !/opr/.test(userAgent)
+    setDevice({
+      type: /mobi|android|iphone|ipod/i.test(userAgent)
+        ? "Mobile"
+        : /ipad|tablet/i.test(userAgent)
+        ? "Tablet"
+        : "Web",
+      os: /windows/.test(userAgent)
+        ? "Windows"
+        : /mac os|macintosh/.test(userAgent)
+        ? "MacOS"
+        : /linux/.test(userAgent)
+        ? "Linux"
+        : /android/.test(userAgent)
+        ? "Android"
+        : /iphone|ipad|ipod/.test(userAgent)
+        ? "iOS"
+        : "Unknown",
+      browser: /edg/.test(userAgent)
+        ? "Edge"
+        : /chrome/.test(userAgent) && !/opr|opera/.test(userAgent)
         ? "Chrome"
         : /firefox/.test(userAgent)
         ? "Firefox"
         : /safari/.test(userAgent) && !/chrome/.test(userAgent)
         ? "Safari"
-        : /edg/.test(userAgent)
-        ? "Edge"
-        : /opera|opr/.test(userAgent)
+        : /opr|opera/.test(userAgent)
         ? "Opera"
-        : "Unknown";
-
-    dispatch(reducer(device));
+        : "Unknown",
+    });
   }, []);
-  return;
+
+  return device;
 };
 
 export default useDeviceInfo;
