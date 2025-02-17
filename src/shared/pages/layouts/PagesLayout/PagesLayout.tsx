54417e4c94ff852e8045a959dc0df5e259e7a8df
nara <parknara0213@gmail.com>
2025-02-17 10:56:36 +0900

    Fix: [수정] 레이아웃 수정
    Refs: #11

--- src/shared/pages/layouts/PagesLayout/PagesLayout.tsx
+++ src/shared/pages/layouts/PagesLayout/PagesLayout.tsx
@@ -1,11 +1,20 @@
 import styles from "./PagesLayout.module.css";
 import { Outlet } from "react-router";
 
-
 const PagesLayout = () => {
-  return <div className={styles[`pages-layout`]}>
-    <Outlet />
-  </div>;
+  return (
+    <div className={styles[`pages__layout`]}>
+      <header role="banner" className={styles["pages__layout__header"]}>
+        사이드 바
+      </header>
+      <main role="main" className={styles["pages__layout__main"]}>
+        <div className={styles[`pages__layout__page`]}>
+          <Outlet />
+        </div>
+        <aside className={styles[`pages__layout__sidebar`]}>기타</aside>
+      </main>
+    </div>
+  );
 };
 
 export default PagesLayout;
