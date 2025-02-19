9a8f2e92eae08dba33841987cff7fc0192f62704
nara <parknara0213@gmail.com>
2025-02-19 17:21:52 +0900

    Feat: [추가] 새로 고침 시 모달창 유지
    Refs: #121

--- src/shared/pages/ui/WritePostModal/WritePostModal.tsx
+++ src/shared/pages/ui/WritePostModal/WritePostModal.tsx
@@ -1,21 +1,32 @@
 import { useAppDispatch } from "@app/store";
 import { useLanguageContent } from "@shared/@common/models/hooks";
 import { getWritePostModal } from "@shared/@common/models/selectors";
-import { onParallelModalClose } from "@shared/@common/models/slices/modalSlice";
+import {
+  onParallelModalClose,
+  onParallelModalOpen,
+} from "@shared/@common/models/slices/modalSlice";
 import { Modal } from "@shared/@common/ui/components";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface WritePostModalProps {}
 
 const WritePostModal = ({}: WritePostModalProps) => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const isOpen = useSelector(getWritePostModal);
+
+  // 새로고침 시 주소가 같은 경우 모달 창 유지 
+  useEffect(() => {
+    if (pathname.includes("/compose/post")) {
+      dispatch(onParallelModalOpen("write"));
+    }
+  }, [isOpen]);
 
   // 언어 설정
   const {} = useLanguageContent(["components", "WritePostModal"]);
-
-  const isOpen = useSelector(getWritePostModal);
 
   const onClose = () => {
     dispatch(onParallelModalClose("write"));
