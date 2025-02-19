119f6894ea8c9cff8d4833b90e48478a1e94308a
nara <parknara0213@gmail.com>
2025-02-19 11:51:24 +0900

    Feat: [추가] NavMoreButton의 위치 저장 => 드롭다운 위치 지정에 쓰임
    Refs: #119

--- src/shared/pages/ui/NavMoreButton/NavMoreButton.tsx
+++ src/shared/pages/ui/NavMoreButton/NavMoreButton.tsx
@@ -3,7 +3,7 @@ import { useLanguageContent } from "@shared/@common/models/hooks";
 import { Dropdown } from "@shared/@common/ui/components";
 import { Icon } from "@shared/@common/ui/icons";
 import { joinClassNames } from "@shared/@common/utils";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface NavMoreButtonProps {
   className?: string;
@@ -11,29 +11,95 @@ interface NavMoreButtonProps {
 }
 
 const NavMoreButton = ({ className, disabled = false }: NavMoreButtonProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState(false);
+  // container의 위치 저장
+  const [rect, setRect] = useState<{
+    top?: number;
+    bottom?: number;
+    left?: number;
+    right?: number;
+  }>({});
+
   // 언어 설정
   const { moreTitle } = useLanguageContent(["components", "NavMoreButton"]);
-
   const classNames = joinClassNames([styles["nav__more__button"], className]);
 
+  // 드롭다운 열기
   const onOpen = () => {
     setIsOpen(true);
   };
-
+  // 드롭다운 닫기
   const onClose = () => setIsOpen(false);
 
+  // 부모 요소의 위치 정보 저장
+  const updateRect = useCallback(() => {
+    if (!containerRef.current) return;
+
+    const { top, bottom, left, right } =
+      containerRef.current.getBoundingClientRect();
+
+    setRect({ top, bottom, right, left });
+  }, []);
+
+  // 부모 요소의 위치 정보 업데이트
+  useEffect(() => {
+    updateRect();
+
+    window.addEventListener("resize", updateRect);
+
+    // containerRef.current와 그 상위 부모 요소에서 스크롤 이벤트 리스닝
+    let currentElement: HTMLElement | null = containerRef.current;
+    while (currentElement) {
+      currentElement.addEventListener("scroll", updateRect);
+      currentElement = currentElement.parentElement;
+    }
+
+    return () => {
+      window.removeEventListener("resize", updateRect);
+
+      // scroll 이벤트 리스너 제거 (상위 요소들을 포함하여)
+      currentElement = containerRef.current;
+      while (currentElement) {
+        currentElement.removeEventListener("scroll", updateRect);
+        currentElement = currentElement.parentElement;
+      }
+    };
+  }, []);
+
   return (
-    <button
-      className={classNames}
-      title={moreTitle}
+    <div
+      className={styles["nav__more__button__container"]}
       onClick={() => (isOpen ? onClose() : onOpen())}
+      ref={containerRef}
     >
-      <Icon iconName="moreRounded" iconSize="2xl" bgColor="transparent" />
-      <Dropdown name="nav" isOpen={isOpen}>
-        하이
-      </Dropdown>
-    </button>
+      <button title={moreTitle} className={classNames}>
+        <Icon iconName="moreRounded" iconSize="2xl" bgColor="transparent" />
+        <Dropdown
+          name="nav"
+          isOpen={isOpen}
+          bottom={
+            rect.bottom && window.innerHeight > rect.bottom
+              ? window.innerHeight - rect.bottom
+              : 0
+          }
+          left={rect.left}
+        >
+          <li>하이1</li>
+          <li>하이2</li>
+          <li>하이3</li>
+          <li>하이4</li>
+          <li>하이5</li>
+          <li>하이6</li>
+          <li>하이7</li>
+          <li>하이8</li>
+          <li>하이9</li>
+          <li>하이10</li>
+          <li>하이11</li>
+          <li>하이12</li>
+        </Dropdown>
+      </button>
+    </div>
   );
 };
 
