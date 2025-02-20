677cb5bf316ccf3d997c9e6b2302c623bd711f34
nara <parknara0213@gmail.com>
2025-02-20 18:26:21 +0900

    Feat: [추가] 화면 너비에 따른 변화 추가 펼친 경우 - 텍스트 추가 - 배지 위치 변화
    Refs: #118

--- src/shared/pages/ui/NavItem/NavItem.tsx
+++ src/shared/pages/ui/NavItem/NavItem.tsx
@@ -2,6 +2,7 @@ import styles from "./NavItem.module.css";
 import { NavLink } from "react-router-dom";
 import { Icon, Icons } from "@shared/@common/ui/icons";
 import CountBadge from "../CountBadge/CountBadge";
+import { Text } from "@shared/@common/ui/components";
 
 interface NavItemProps {
   to: string;
@@ -21,13 +22,16 @@ const NavItem = ({
   return (
     <NavLink to={to} className={styles[`nav__item`]} title={title}>
       {({ isActive }) => (
-        <div>
+        <div className={styles[`nav__item__wrapper`]}>
           <Icon
             iconName={isActive ? activeIconName : inactiveIconName}
             iconSize="2xl"
             bgColor="transparent"
           />
-          {count && count > 0 && <CountBadge count={count} />}
+          {count && count > 0 && (
+            <CountBadge count={count} className={styles[`nav__item__badge`]} />
+          )}
+          <Text className={styles["nav__item__text"]}>{title}</Text>
         </div>
       )}
     </NavLink>
