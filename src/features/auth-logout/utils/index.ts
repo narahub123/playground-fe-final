3a6dddf6be5b24fe3e146b61bbcc290d0b0b7927
nara <parknara0213@gmail.com>
2025-02-21 12:11:52 +0900

    Feat: [추가] access 토큰 제거 함수 생성
    Refs: #125

---
+++ src/features/auth-logout/utils/index.ts
@@ -0,0 +1,3 @@
+import removeAccessToken from "./removeAccessToken";
+
+export { removeAccessToken };
---
+++ src/features/auth-logout/utils/removeAccessToken.ts
@@ -0,0 +1,5 @@
+const removeAccessToken = () => {
+  localStorage.removeItem("accessToken");
+};
+
+export default removeAccessToken;
