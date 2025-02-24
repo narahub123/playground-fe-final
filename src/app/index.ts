92951bed25a155d73673461a43321f16645ab263
nara <parknara0213@gmail.com>
2025-02-24 14:26:10 +0900

    Fix: [수정] - router.tsx 삭제
    Refs: #1

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,8 @@
-import router from "./router";
 import guestRouter from "./guestRouter";
 import userRouter from "./userRouter";
 import store from "./store";
 
 export {
-  router, // 라우터
   guestRouter, // 방문자 라우터
   userRouter, // 사용자 라우터
   store, // 스토어
