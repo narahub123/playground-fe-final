0d15f0421331888b79ff7cbe73a5a12fd320dc3e
nara <parknara0213@gmail.com>
2025-02-25 00:06:36 +0900

    Feat: [추가] SettingsPage 기본 레이아웃 생성
    Refs: #139

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,6 +1,9 @@
 import styles from "./SettingsPage.module.css";
 import { useLanguageContent } from "@shared/@common/models/hooks";
+import { Text } from "@shared/@common/ui/components";
 import { joinClassNames } from "@shared/@common/utils";
+import { LuChevronRight } from "react-icons/lu";
+import { NavLink } from "react-router-dom";
 
 interface SettingsPageProps {
   className?: string;
@@ -13,7 +16,40 @@ const SettingsPage = ({ className, disabled = false }: SettingsPageProps) => {
 
   const classNames = joinClassNames([styles["settings__page"], className]);
 
-  return <div className={classNames}>SettingsPage</div>;
+  return (
+    <div className={classNames}>
+      <section className={styles[`settings___explore__section`]}>
+        <header className={styles["section__header"]}>
+          <Text type="heading3">설정</Text>
+        </header>
+        <div className={styles["section__header__content"]}>헤더 컨텐츠</div>
+        <main className={styles["section__main"]}>
+          <div role="tablist" className={styles[`section__tab__list`]}>
+            <NavLink
+              to="account"
+              role="tabpanel"
+              className={({ isActive }) =>
+                joinClassNames([
+                  styles[`section__tab`],
+                  isActive ? styles[`section__tab--active`] : "",
+                ])
+              }
+            >
+              <Text>계정</Text>
+              <LuChevronRight />
+            </NavLink>
+          </div>
+        </main>
+      </section>
+      <section className={styles[`settings___details__section`]}>
+        <header className={styles["section__header"]}>
+          <Text type="heading3">설정</Text>
+        </header>
+        <div className={styles["section__header__content"]}>헤더 컨텐츠</div>
+        <main className={styles["section__main"]}>메인</main>
+      </section>
+    </div>
+  );
 };
 
 export default SettingsPage;
