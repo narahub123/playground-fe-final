f1db5062150fb5da45ce35ba844f0662d1355324
nara <parknara0213@gmail.com>
2025-02-26 17:37:12 +0900

    Fix: [수정] sectionDataMap 수정 - 상대 경로를 절대 경로로 수정
    Refs: #139

--- src/shared/pages/settings/data/sectionDataMap.ts
+++ src/shared/pages/settings/data/sectionDataMap.ts
@@ -21,99 +21,99 @@ const sectionDataMap: ISectionDataMap = {
   },
   AccountSection: {
     accountInfo: {
-      link: SETTINGS_LINKS.ACCOUNT_INFO,
+      link: `/settings/${SETTINGS_LINKS.ACCOUNT_INFO}`,
       iconName: "userLine",
     },
     password: {
-      link: SETTINGS_LINKS.PASSWORD,
+      link: `/settings/${SETTINGS_LINKS.PASSWORD}`,
       iconName: "keyIcon",
     },
     downloadData: {
-      link: SETTINGS_LINKS.DOWNLOAD_DATA,
+      link: `/settings/${SETTINGS_LINKS.DOWNLOAD_DATA}`,
       iconName: "downloadIcon",
     },
     deactivate: {
-      link: SETTINGS_LINKS.DEACTIVATE,
+      link: `/settings/${SETTINGS_LINKS.DEACTIVATE}`,
       iconName: "deactivateIcon",
     },
   },
   SecurityAndAccountAccessSection: {
     security: {
-      link: SETTINGS_LINKS.SECURITY,
+      link: `/settings/${SETTINGS_LINKS.SECURITY}`,
       iconName: "securityIcon",
     },
     appsAndSessions: {
-      link: SETTINGS_LINKS.APPS_AND_SESSIONS,
+      link: `/settings/${SETTINGS_LINKS.APPS_AND_SESSIONS}`,
       iconName: "appsIcon",
     },
     connectedAccounts: {
-      link: SETTINGS_LINKS.CONNECTED_ACCOUNTS,
+      link: `/settings/${SETTINGS_LINKS.CONNECTED_ACCOUNTS}`,
       iconName: "connectedAccountsIcon",
     },
     delegate: {
-      link: SETTINGS_LINKS.DELEGATE,
+      link: `/settings/${SETTINGS_LINKS.DELEGATE}`,
       iconName: "delegateIcon",
     },
   },
   PrivacyAndSafetySection: {
     audienceAndTagging: {
-      link: SETTINGS_LINKS.AUDIENCE_AND_TAGGING,
+      link: `/settings/${SETTINGS_LINKS.AUDIENCE_AND_TAGGING}`,
       iconName: "audienceIcon",
     },
     myPosts: {
-      link: SETTINGS_LINKS.MY_POSTS,
+      link: `/settings/${SETTINGS_LINKS.MY_POSTS}`,
       iconName: "myPostsIcon",
     },
     contentISee: {
-      link: SETTINGS_LINKS.CONTENT_I_SEE,
+      link: `/settings/${SETTINGS_LINKS.CONTENT_I_SEE}`,
       iconName: "contentIcon",
     },
     muteAndBlock: {
-      link: SETTINGS_LINKS.MUTE_AND_BLOCK,
+      link: `/settings/${SETTINGS_LINKS.MUTE_AND_BLOCK}`,
       iconName: "muteIcon",
     },
     directMessages: {
-      link: SETTINGS_LINKS.DIRECT_MESSAGES,
+      link: `/settings/${SETTINGS_LINKS.DIRECT_MESSAGES}`,
       iconName: "envelopLine",
     },
     contacts: {
-      link: SETTINGS_LINKS.CONTACTS,
+      link: `/settings/${SETTINGS_LINKS.CONTACTS}`,
       iconName: "findPersonIcon",
     },
     adsPreferences: {
-      link: SETTINGS_LINKS.ADS_PREFERENCES,
+      link: `/settings/${SETTINGS_LINKS.ADS_PREFERENCES}`,
       iconName: "adsPreferencesIcon",
     },
     locationInfo: {
-      link: SETTINGS_LINKS.LOCATION_INFORMATION,
+      link: `/settings/${SETTINGS_LINKS.LOCATION_INFORMATION}`,
       iconName: "pinIcon",
     },
   },
   NotificationsSection: {
     notification_filters: {
-      link: SETTINGS_LINKS.NOTIFICATIONS_FILTERS,
+      link: `/settings/${SETTINGS_LINKS.NOTIFICATIONS_FILTERS}`,
       iconName: "notificationFiltersIcon",
     },
     notification_preferences: {
-      link: SETTINGS_LINKS.NOTIFICATIONS_PREFERENCES,
+      link: `/settings/${SETTINGS_LINKS.NOTIFICATIONS_PREFERENCES}`,
       iconName: "notificationPreferencesIcon",
     },
   },
   AccessibiltyAndDisplayAndLanguagesSection: {
     accessibility: {
-      link: SETTINGS_LINKS.ACCESSIBILITY,
+      link: `/settings/${SETTINGS_LINKS.ACCESSIBILITY}`,
       iconName: "accessibilityIcon",
     },
     display: {
-      link: SETTINGS_LINKS.DISPLAY,
+      link: `/settings/${SETTINGS_LINKS.DISPLAY}`,
       iconName: "displayIcon",
     },
     language: {
-      link: SETTINGS_LINKS.LANGUAGE,
+      link: `/settings/${SETTINGS_LINKS.LANGUAGE}`,
       iconName: "languageIcon",
     },
     data: {
-      link: SETTINGS_LINKS.DATA,
+      link: `/settings/${SETTINGS_LINKS.DATA}`,
       iconName: "dataIcon",
     },
   },
