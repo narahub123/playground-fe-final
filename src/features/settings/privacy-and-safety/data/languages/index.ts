7811f068d4f8674866b2cbc71de278d8a4afc138
nara <parknara0213@gmail.com>
2025-03-02 00:04:11 +0900

    Fix: [수정] privacya and safety lang 수정
    Refs: #161

--- src/features/settings/privacy-and-safety/data/languages/index.ts
+++ src/features/settings/privacy-and-safety/data/languages/index.ts
@@ -1,11 +1,15 @@
 import { privacy_and_safety_us } from "./en_US";
+import { privacy_and_safety_jp } from "./ja_JP";
+import { privacy_and_safety_kr } from "./ko_KR";
+import { privacy_and_safety_cn } from "./zh_CN";
+import { privacy_and_safety_tw } from "./zh_TW";
 
 const privacy_and_safety_lang = {
   us: privacy_and_safety_us,
-  jp: privacy_and_safety_us,
-  kr: privacy_and_safety_us,
-  cn: privacy_and_safety_us,
-  tw: privacy_and_safety_us,
+  jp: privacy_and_safety_jp,
+  kr: privacy_and_safety_kr,
+  cn: privacy_and_safety_cn,
+  tw: privacy_and_safety_tw,
 };
 
 export { privacy_and_safety_lang };
