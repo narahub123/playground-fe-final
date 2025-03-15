3245c18887d3e6473a9ede4cd212a57f33459f51
nara <parknara0213@gmail.com>
2025-03-15 15:36:25 +0900

    Fix: [수정] useUpdateLines 수정 - updateLines 의존성 추가 - Array.prototype.slice.call을 Array.from으로 변경 - log 추가
    Refs: #175

--- src/shared/pages/ui/PostEditor/ui/TextEditor/hooks/useUpdateLines.tsx
+++ src/shared/pages/ui/PostEditor/ui/TextEditor/hooks/useUpdateLines.tsx
@@ -1,35 +1,49 @@
 import { useCallback } from "react";
-import { ILine, ISegment } from "../types";
-import { getRowAndColOfSegment, getSegmentType } from "../utils";
-import { initialLine } from "../data";
+import {
+  ILine,
+  ISegment,
+  getRowAndColOfSegment,
+  getSegmentType,
+  initialLine,
+} from "@shared/pages/ui/PostEditor/ui/TextEditor";
 
 const useUpdateLines = (
   linesRef: React.MutableRefObject<(HTMLDivElement | null)[]>,
   setLines: React.Dispatch<React.SetStateAction<ILine[]>>
 ) => {
   const updateLines = useCallback(() => {
+    console.log(
+      "--------------------- updateLines 시작 --------------------------"
+    );
+
     const lines = linesRef.current;
 
     if (lines.length === 0) return;
 
+    console.log("현재 linesRef.current:", lines); // linesRef의 내용을 로그로 기록
+
     const newLines = [...lines];
 
     const updatedLines: ILine[] = newLines.map((line, idx) => {
-      console.log("각 행", line);
       if (!line) return initialLine;
 
-      const segments = Array.prototype.slice.call(line.childNodes);
-      // const segments = Array.from(line.childNodes);
+      const segments = Array.from(line.childNodes);
+
+      console.log(`라인 ${idx}의 세그먼트들:`, segments); // 각 라인의 세그먼트 정보 로그
 
       const updatedSegments: ISegment[] = segments.map((segment) => {
         const { row, col } = getRowAndColOfSegment(segment);
 
-        return {
+        const segmentData = {
           type: getSegmentType(segment),
           row,
           col,
-          text: segment.textContent,
+          text: segment.textContent || "",
         };
+
+        console.log("세그먼트 데이터:", segmentData); // 각 세그먼트의 데이터 로그
+
+        return segmentData;
       });
 
       return {
@@ -38,10 +52,13 @@ const useUpdateLines = (
       };
     });
 
-    console.log("업데이트된 줄들", updatedLines);
+    console.log("업데이트된 줄들:", updatedLines); // 최종적으로 업데이트된 라인 데이터 로그
 
     setLines(updatedLines);
-  }, []);
+    console.log(
+      "--------------------- updateLines 완료 --------------------------"
+    );
+  }, [linesRef, setLines]);
 
   return updateLines;
 };
