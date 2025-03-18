e07388eb7de14153ee1dc976e4c63ffba09e3c70
nara <parknara0213@gmail.com>
2025-03-18 12:46:12 +0900

    Fix: [수정] VoteButton handleClick 적용
    Refs: #175

--- src/shared/pages/ui/PostEditor/models/slices/index.ts
+++ src/shared/pages/ui/PostEditor/models/slices/index.ts
@@ -1,3 +1,19 @@
 import postEditorSlice from "./postEditorSlice";
 
+import {
+  clearPostEditor,
+  setPostEditorMedia,
+  removePostEditorMedia,
+  postEditorToolbarButtonOff,
+  postEditorToolbarButtonOn,
+} from "./reducers";
+
 export { postEditorSlice };
+
+export {
+  clearPostEditor,
+  setPostEditorMedia,
+  removePostEditorMedia,
+  postEditorToolbarButtonOff,
+  postEditorToolbarButtonOn,
+};
--- src/shared/pages/ui/PostEditor/ui/PostEditorToolbar/VoteButton/VoteButton.tsx
+++ src/shared/pages/ui/PostEditor/ui/PostEditorToolbar/VoteButton/VoteButton.tsx
@@ -1,17 +1,23 @@
-import { useLanguageContent } from "@shared/@common/models/hooks";
-import ToolbarButton from "../ToolbarButton/ToolbarButton";
+import { useAppDispatch } from "@app/store";
 import { RiListRadio } from "react-icons/ri";
+import { useLanguageContent } from "@shared/@common/models/hooks";
+import { ToolbarButton } from "@shared/pages/ui/PostEditor/ui/PostEditorToolbar";
+import { postEditorToolbarButtonOn } from "@shared/pages/ui/PostEditor/models/slices";
 
 interface VoteButtonProps {
   disabled?: boolean;
 }
 
 const VoteButton = ({ disabled = false }: VoteButtonProps) => {
+  const dispatch = useAppDispatch();
   // 언어 설정
   const { title } = useLanguageContent(["components", "VoteButton"]);
 
+  const handleClick = () => {
+    dispatch(postEditorToolbarButtonOn("vote"));
+  };
   return (
-    <ToolbarButton onClick={() => {}} title={title} disabled={disabled}>
+    <ToolbarButton onClick={handleClick} title={title} disabled={disabled}>
       <RiListRadio fontSize={"1.25rem"} />
     </ToolbarButton>
   );
