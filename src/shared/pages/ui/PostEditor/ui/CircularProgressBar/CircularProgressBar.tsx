4370d1f73ffea7ae2a3a5b71a56ebdfaca16135d
nara <parknara0213@gmail.com>
2025-04-08 12:31:38 +0900

    Fix: [수정] CircularProgressBar 수정 - invisible, warning, error 구간 도입 - 텍스트 삽입
    Refs: #175

--- src/shared/pages/ui/PostEditor/ui/CircularProgressBar/CircularProgressBar.tsx
+++ src/shared/pages/ui/PostEditor/ui/CircularProgressBar/CircularProgressBar.tsx
@@ -16,25 +16,49 @@ const CircularProgressBar = ({
     className,
   ]);
 
+  const warningCond =
+    POST_LENGTH_MAX - textLength <= 10 && POST_LENGTH_MAX - textLength > 0;
+  const errorCond = POST_LENGTH_MAX - textLength <= 0;
+  const textInvisibleCond = POST_LENGTH_MAX - textLength > 10;
+  const svgInvisibleCond = POST_LENGTH_MAX - textLength < -4;
+
   return (
     <div className={classNames}>
+      <span
+        className={joinClassNames([
+          styles["text"],
+          warningCond ? styles["text--warning"] : "",
+          errorCond ? styles["text--error"] : "",
+          textInvisibleCond ? styles["text--invisible"] : "",
+        ])}
+      >
+        {POST_LENGTH_MAX - textLength}
+      </span>
       <svg width={30} height={30} className={styles["svg"]}>
-        <circle r={10} cx={15} cy={15} className={styles["progress"]}></circle>
         <circle
-          r={10}
+          r={12}
+          cx={15}
+          cy={15}
+          className={joinClassNames([
+            styles["progress"],
+            warningCond ? styles["progress--warning"] : "",
+            errorCond ? styles["progress--error"] : "",
+            svgInvisibleCond ? styles["progress--invisible"] : "",
+          ])}
+        />
+        <circle
+          r={12}
           cx={15}
           cy={15}
           className={styles["border"]}
           style={{
-            strokeDasharray: 2 * Math.PI * 10,
-            strokeDashoffset: `${-(
-              (textLength / POST_LENGTH_MAX) *
-              2 *
-              Math.PI *
-              10
+            strokeDasharray: 2 * Math.PI * 12,
+            strokeDashoffset: `${-Math.min(
+              (textLength / POST_LENGTH_MAX) * 2 * Math.PI * 12,
+              2 * Math.PI * 12
             )}`,
           }}
-        ></circle>
+        />
       </svg>
     </div>
   );
