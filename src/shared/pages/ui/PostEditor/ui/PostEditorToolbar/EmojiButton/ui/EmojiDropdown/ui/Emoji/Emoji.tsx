bad1566df2be1ec66b4be03ee51325df7a099b5a
nara <parknara0213@gmail.com>
2025-04-09 12:07:48 +0900

    Feat: [추가] API를 이용한 최근 이모지 추가
    Refs: #175

--- src/shared/pages/ui/PostEditor/ui/PostEditorToolbar/EmojiButton/ui/EmojiDropdown/ui/Emoji/Emoji.tsx
+++ src/shared/pages/ui/PostEditor/ui/PostEditorToolbar/EmojiButton/ui/EmojiDropdown/ui/Emoji/Emoji.tsx
@@ -11,6 +11,10 @@ import { setEmoji } from "@shared/pages/ui/PostEditor/models/slices/postEditorSl
 import { useSelector } from "react-redux";
 import { selectSkintoneType } from "@shared/@common/models/selectors";
 import { setRecentEmojis } from "@shared/@common/models/slices/userSlice";
+import { fetchWithAuth } from "@shared/pages/utils";
+import { useToast } from "@shared/@common/ui/components/Toast/hooks";
+import { useAPIError } from "@shared/@common/models/hooks";
+import { ErrorDescriptionCodeType } from "@shared/@common/types";
 
 interface EmojiProps {
   className?: string;
@@ -33,16 +37,41 @@ const Emoji = ({ className, disabled = false, emoji }: EmojiProps) => {
     setCurEmoji(null);
   };
 
+  const toast = useToast();
+  const { getErrorDescription } = useAPIError();
+
+  const handleClick = async () => {
+    // 텍스트에 이모지 추가
+    const selectedEmoji = getEmojiWithSkinTone(emoji, skintoneType);
+    dispatch(setEmoji(selectedEmoji));
+
+    // 최근 이모지 목록에 추가
+    const result = await fetchWithAuth(
+      "/users/me",
+      { method: "PATCH" },
+      { recentEmoji: emoji }
+    );
+    try {
+      if (result.success) {
+        dispatch(setRecentEmojis(emoji));
+      } else {
+        const errorCode = Object.values(
+          result.error.details
+        )[0] as ErrorDescriptionCodeType;
+
+        if (errorCode === "UPDATE_RECENT_EMOJIS_FAILED") {
+          toast({
+            description: getErrorDescription(errorCode),
+            type: "error",
+          });
+        }
+      }
+    } catch (error) {}
+  };
+
   return (
     <Button
-      onClick={() => {
-        // 최근 이모지 목록에 추가
-        dispatch(setRecentEmojis(emoji));
-
-        // 텍스트에 이모지 추가
-        const selectedEmoji = getEmojiWithSkinTone(emoji, skintoneType);
-        dispatch(setEmoji(selectedEmoji));
-      }}
+      onClick={handleClick}
       isValid={!disabled}
       className={classNames}
       aria-label={emoji.name}
