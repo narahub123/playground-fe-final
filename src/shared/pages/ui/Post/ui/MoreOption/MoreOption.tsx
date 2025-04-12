aba7de08c0ab231f17cfc6d716536cbc59001339
nara <parknara0213@gmail.com>
2025-04-12 15:58:41 +0900

    Feat: [추가] handleClick를 러프하게 작성함 사용자 본인인 경우를 완료하고 완료 예정
    Refs: #175

--- src/shared/pages/ui/Post/ui/MoreOption/MoreOption.tsx
+++ src/shared/pages/ui/Post/ui/MoreOption/MoreOption.tsx
@@ -38,8 +38,43 @@ const MoreOption = ({
       ? isBlocking(userId)
       : undefined;
 
+  // 나중에 hook으로 변경할 것
+  const handleClick = () => {
+    switch (option) {
+      case "following":
+        // 팔로잉 중인 경우 언팔로우, 팔로잉을 하지 않는 경우 팔로잉
+        break;
+      case "list":
+        // 리스트에 있는 경우 삭제, 없는 경우 추가
+        break;
+      case "mute":
+        // mute 중인 경우 뮤트 해제, 뮤트 중이 아닌 경우 뮤트
+        break;
+      case "block":
+        // 차단 중인 경우 차단 해제, 차단 중이 아닌 경우 차단
+        break;
+      case "view":
+        // post 페이지 url/quote로 이동해서 통계를 보여줌
+        break;
+      case "embed":
+        // embed 페이지로 이동
+        break;
+      case "report":
+        // 신고 모달을 엶
+        break;
+      case "groupNote":
+        // communityNote 모달을 엶
+        break;
+    }
+  };
+
   return (
-    <button className={classNames} type="button" disabled={disabled}>
+    <button
+      className={classNames}
+      type="button"
+      disabled={disabled}
+      onClick={handleClick}
+    >
       <MoreOptionIcon option={option} toggle={toggle} />
       <Text>{text(option, userId, toggle)}</Text>
     </button>
