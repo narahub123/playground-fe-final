fbfb5fc5bee72a172b77b6ac1b5335b282bfaca0
nara <parknara0213@gmail.com>
2025-04-12 13:40:18 +0900

    Feat: [추가] MoreButton 클릭 시 드롭다운을 여닫고, 드롭다운의 위치를 지정할 수 있는 상태 및 함수 생성
    Refs: #175

--- src/shared/pages/ui/Post/ui/MoreButton/MoreButton.tsx
+++ src/shared/pages/ui/Post/ui/MoreButton/MoreButton.tsx
@@ -6,17 +6,24 @@ import { IoIosMore } from "react-icons/io";
 
 interface MoreButtonProps {
   className?: string;
+  onClick: () => void;
 }
 
 const MoreButton = forwardRef<HTMLButtonElement, MoreButtonProps>(
-  ({ className }, ref) => {
+  ({ className, onClick }, ref) => {
     // 언어 설정
     const { title } = useLanguageContent(["post", "MoreButton"]);
 
     const classNames = joinClassNames([styles["more__button"], className]);
 
     return (
-      <button className={classNames} ref={ref} data-title={title} type="button">
+      <button
+        className={classNames}
+        ref={ref}
+        data-title={title}
+        type="button"
+        onClick={onClick}
+      >
         <IoIosMore className={styles["icon"]} aria-hidden={true} />
       </button>
     );
--- src/shared/pages/ui/Post/ui/MoreMenu/MoreMenu.tsx
+++ src/shared/pages/ui/Post/ui/MoreMenu/MoreMenu.tsx
@@ -1,7 +1,7 @@
 import styles from "./MoreMenu.module.css";
 import { joinClassNames } from "@shared/@common/utils";
-import { MoreButton } from "@shared/pages/ui/Post";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { IRect, MoreButton } from "@shared/pages/ui/Post";
 
 interface MoreMenuProps {
   className?: string;
@@ -10,10 +10,45 @@ interface MoreMenuProps {
 const MoreMenu = ({ className }: MoreMenuProps) => {
   const classNames = joinClassNames([styles["more__menu"], className]);
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [rect, setRect] = useState<IRect>();
+
+  // 드롭다운 위치 지정
+  useEffect(() => {
+    const getButtonPosition = () => {
+      if (!buttonRef.current) return;
+      const button = buttonRef.current;
+
+      const { top, bottom, left, right } = button.getBoundingClientRect();
+
+      setRect({ top, bottom, left, right });
+    };
+
+    getButtonPosition();
+
+    window.addEventListener("resize", getButtonPosition);
+    window.addEventListener("scroll", getButtonPosition);
+
+    return () => {
+      window.removeEventListener("resize", getButtonPosition);
+      window.removeEventListener("scroll", getButtonPosition);
+    };
+  }, []);
+
+  const onOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  console.log(isOpen);
+  console.log(rect);
 
   return (
     <div className={classNames}>
-      <MoreButton ref={buttonRef} />
+      <MoreButton ref={buttonRef} onClick={onOpen} />
     </div>
   );
 };
