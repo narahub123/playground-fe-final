34f64d2925a29d50b90c3aeedd2ae704b6da70b0
nara <parknara0213@gmail.com>
2025-04-14 10:04:22 +0900

    Fix: [수정] CoFollowings 삭제
    Refs: #175

--- src/shared/pages/ui/Post/ui/CoFollowings/CoFollowings.tsx
+++ src/shared/pages/ui/Post/ui/CoFollowings/CoFollowings.tsx
@@ -2,12 +2,15 @@ import styles from "./CoFollowings.module.css";
 import { useLanguageContent } from "@shared/@common/models/hooks";
 import { ProfileImage } from "@shared/@common/ui/components";
 import { joinClassNames } from "@shared/@common/utils";
+import { useNavigate } from "react-router-dom";
 
 interface CoFollowingsProps {
   className?: string;
+  userId: string;
 }
 
-const CoFollowings = ({ className }: CoFollowingsProps) => {
+const CoFollowings = ({ className, userId }: CoFollowingsProps) => {
+  const navigate = useNavigate();
   // 언어 설정
   const { text } = useLanguageContent(["post", "CoFollowings"]);
 
@@ -15,21 +18,38 @@ const CoFollowings = ({ className }: CoFollowingsProps) => {
 
   const followings = [1, 2, 3];
 
+  const handleClick = () => {
+    navigate(`/${userId}/followers_you_follow`);
+  };
+
   return (
     <div className={classNames}>
-      <span className={styles["images"]}>
-        {followings.map((_, index) => (
-          <ProfileImage
-            width={"2rem"}
-            height={"2rem"}
-            rounded="full"
-            key={index}
-            className={styles["image"]}
-            style={{ position: "absolute", left: `${index * 10}px` }}
-          />
+      <div
+        className={styles["images"]}
+        style={{
+          width: `${2 + 0.5 * (followings.length - 1)}rem`,
+          height: `2rem`,
+        }}
+        onClick={handleClick}
+      >
+        {followings.map((f, index) => (
+          <div
+            className={styles["image__wrapper"]}
+            style={{
+              left: `${0.5 * index}rem`,
+              zIndex: `${followings.length - index}`,
+            }}
+          >
+            <ProfileImage
+              width={"2rem"}
+              height={"2rem"}
+              rounded="full"
+              className={styles["image"]}
+            />
+          </div>
         ))}
-      </span>
-      <span className={styles["text"]}>{text([])}</span>
+      </div>
+      <div className={styles["text"]}>{text([])}</div>
     </div>
   );
 };
