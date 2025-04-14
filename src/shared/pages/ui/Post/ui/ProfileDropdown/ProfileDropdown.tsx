ae2e113bd4bd958f4a392bbd282b330954576347
nara <parknara0213@gmail.com>
2025-04-14 13:15:54 +0900

    Feat: [추가] ProfileDropdown - 요소별 링크 추가 Refs: #175

--- src/shared/pages/ui/Post/ui/ProfileDropdown/ProfileDropdown.tsx
+++ src/shared/pages/ui/Post/ui/ProfileDropdown/ProfileDropdown.tsx
@@ -10,10 +10,13 @@ import {
   useUserRelationStatus,
 } from "@shared/pages/ui/Post";
 import { defaultProfileImage } from "@shared/@common/assets";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface ProfileDropdownProps {}
 
 const ProfileDropdown = ({}: ProfileDropdownProps) => {
+  const navigate = useNavigate();
   // 언어 설정
   const { stats } = useLanguageContent(["post", "ProfileDropdown"]);
   const classNames = joinClassNames([styles["profile__dropdown"]]);
@@ -40,6 +43,8 @@ const ProfileDropdown = ({}: ProfileDropdownProps) => {
             height={"4rem"}
             rounded="full"
             src={profileImage || defaultProfileImage}
+            className={styles["image"]}
+            onClick={() => navigate(`/${userId}`)}
           />
           {/* 본인의 계정의 경우 보이지 않음 */}
 
@@ -49,8 +54,13 @@ const ProfileDropdown = ({}: ProfileDropdownProps) => {
           }
         </div>
         <div className={styles["info"]}>
-          <Text className={styles["username"]}>{username}</Text>
-          <Text className={styles["userId"]}>{`@${userId}`}</Text>
+          <Link to={`/${userId}`} className={styles["link"]}>
+            <Text className={styles["username"]}>{username}</Text>
+          </Link>
+          <Text
+            className={styles["userId"]}
+            onClick={() => navigate(`/${userId}`)}
+          >{`@${userId}`}</Text>
         </div>
         {/* 유저에 소개글이 없는 경우 표시 안됨 */}
         {intro && (
@@ -59,24 +69,27 @@ const ProfileDropdown = ({}: ProfileDropdownProps) => {
           </div>
         )}
         <div className={styles["stats"]}>
-          <Text className={styles["followings"]}>
-            <em className={styles["emphasis"]}>{`${formatNumber(
-              followings.length
-            )}`}</em>
-            <span className={styles["unit"]}>{` ${stats.followings}`}</span>
-          </Text>
-          <Text className={styles["followers"]}>
-            <em className={styles["emphasis"]}>{`${formatNumber(
-              followers.length
-            )}`}</em>
-            <span className={styles["unit"]}>{` ${stats.followers}`}</span>
-          </Text>
+          <Link to={`/${userId}/followings`} className={styles["link"]}>
+            <Text className={styles["followings"]}>
+              <em className={styles["emphasis"]}>{`${formatNumber(
+                followings.length
+              )}`}</em>
+              <span className={styles["unit"]}>{` ${stats.followings}`}</span>
+            </Text>
+          </Link>
+          <Link to={`/${userId}/verified_followers`} className={styles["link"]}>
+            <Text className={styles["followers"]}>
+              <em className={styles["emphasis"]}>{`${formatNumber(
+                followers.length
+              )}`}</em>
+              <span className={styles["unit"]}>{` ${stats.followers}`}</span>
+            </Text>
+          </Link>
         </div>
         {/* 본인의 계정의 경우 보이지 않음 */}
-        {
-          // !isMyself(userId) &&
+        {!isMyself(userId) && (
           <CoFollowers userId={userId} followers={followers} />
-        }
+        )}
       </div>
     </Dropdown>
   );
