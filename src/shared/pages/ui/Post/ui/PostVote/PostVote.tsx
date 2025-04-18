9785a5bcd3c18b3fd9acf5c9996367f3cb02c77e
nara <parknara0213@gmail.com>
2025-04-18 12:32:34 +0900

    Feat: [추가] PostVote 레이아웃
    Refs: #175

--- src/shared/pages/ui/Post/ui/PostVote/PostVote.tsx
+++ src/shared/pages/ui/Post/ui/PostVote/PostVote.tsx
@@ -1,6 +1,10 @@
 import styles from "./PostVote.module.css";
 import { useLanguageContent } from "@shared/@common/models/hooks";
+import { Button, Text } from "@shared/@common/ui/components";
 import { joinClassNames } from "@shared/@common/utils";
+import { usePostContext } from "@shared/pages/ui/Post";
+import { useState } from "react";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
 interface PostVoteProps {
   className?: string;
@@ -10,9 +14,88 @@ const PostVote = ({ className }: PostVoteProps) => {
   // 언어 설정
   //   const {} = useLanguageContent(["", "PostVote"]);
 
+  const { vote } = usePostContext();
+
+  if (!vote) return null;
+
+  const { options, duration } = vote;
+
+  // 투표를 했는지 여부 상태
+  const [hasVoted, setHasVoted] = useState(false);
+
   const classNames = joinClassNames([styles["post__vote"], className]);
 
-  return <div className={classNames}>PostVote</div>;
+  const handleVote = () => {
+    console.log("누름");
+    setHasVoted(true);
+  };
+
+  return (
+    <div className={classNames}>
+      <div className={styles["main"]}>
+        <ul className={styles["list"]}>
+          {options.map((option, index) => (
+            <li className={styles["result"]} key={index}>
+              <div
+                className={styles["background"]}
+                style={{
+                  // width: `${option.count === 0 ? "0.5rem" : "100%"}`,
+                  width: `100%`,
+                  backgroundColor: `#ccc`,
+                }}
+              />
+              <div className={styles["wrapper"]}>
+                <div className={styles["left"]}>
+                  <Text className={styles["option__text"]}>
+                    {option.option}
+                  </Text>
+                  <IoIosCheckmarkCircleOutline className={styles["icon"]} />
+                </div>
+                <div className={styles["right"]}>
+                  <Text>{`${option.count}%`}</Text>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {/* {hasVoted ? (
+          <ul className={styles["list"]}>
+            {options.map((option, index) => (
+              <li className={styles["result"]} key={index}>
+                <div className={styles["left"]}>
+                  <Text>{option.option}</Text>
+                  <IoIosCheckmarkCircleOutline className={styles["icon"]} />
+                </div>
+                <div className={styles["right"]}>
+                  <Text>{option.count}</Text>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <ul className={styles["list"]}>
+            {options.map((option, index) => (
+              <li className={styles["option"]} key={index}>
+                <Button
+                  isValid
+                  onClick={handleVote}
+                  className={styles["option__btn"]}
+                  variant="outline"
+                  rounded="2xl"
+                  fontColor="colorTheme"
+                >
+                  {option.option}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        )} */}
+      </div>
+      <div className={styles["text__wrapper"]}>
+        <Text className={styles["stats"]}>{`표 · 남은 시간`}</Text>
+      </div>
+    </div>
+  );
 };
 
 export default PostVote;
