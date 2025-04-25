76770e73e32b71625ea9a7673bdc68ebf0ab889a
nara <parknara0213@gmail.com>
2025-04-25 09:49:16 +0900

    Feat: [추가] PostPage 기본 레이아웃 설정
    Refs: #180

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,7 +1,16 @@
+import { Outlet } from "react-router-dom";
 import styles from "./PostPage.module.css";
 
 const PostPage = () => {
-  return <div className={styles["post__page"]}>PostPage</div>;
+  return (
+    <div className={styles["post__page"]}>
+      <header>
+        <span>뒤로가기 아이콘</span>
+        <span>게시물</span>
+      </header>
+      <Outlet />
+    </div>
+  );
 };
 
 export default PostPage;
