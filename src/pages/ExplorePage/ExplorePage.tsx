18f56914d66cba599fa37be9645e555e1c14dfc5
nara <parknara0213@gmail.com>
2025-05-16 16:43:53 +0900

    Feat: [추가] ExplorePage - api 연결
    Refs: #187

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,19 +1,53 @@
+import { useAppDispatch } from "@app/store";
 import styles from "./ExplorePage.module.css";
 import { useLanguageContent } from "@shared/@common/models/hooks";
 import { joinClassNames } from "@shared/@common/utils";
+import { fetchWithAuth } from "@shared/pages";
+import React, { useState } from "react";
+import { setPosts } from "@shared/@common/models/slices/feedSlice";
+import { useSelector } from "react-redux";
+import { selectPage } from "@shared/@common/models/selectors";
 
 interface ExplorePageProps {
   className?: string;
-  disabled?: boolean;
 }
 
-const ExplorePage = ({ className, disabled = false }: ExplorePageProps) => {
+const ExplorePage = ({ className }: ExplorePageProps) => {
+  const dispatch = useAppDispatch();
   // 언어 설정
   const {} = useLanguageContent(["pages", "ExplorePage"]);
 
+  const page = useSelector(selectPage);
+
+  const [keyword, setKeyword] = useState("");
+
   const classNames = joinClassNames([styles["explore__page"], className]);
 
-  return <div className={classNames}>ExplorePage</div>;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const keyword = e.target.value;
+
+    setKeyword(keyword);
+  };
+
+  const handleSearch = async () => {
+    try {
+      const result = await fetchWithAuth(
+        `/posts/search?q=${keyword}&skip=${page}`
+      );
+
+      if (result.success) {
+        dispatch(setPosts(result.data.posts));
+      } else {
+      }
+    } catch (error) {}
+  };
+
+  return (
+    <div className={classNames}>
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleSearch}>검색</button>
+    </div>
+  );
 };
 
 export default ExplorePage;
