0f8d19765454ef771c8989a749f439893daf823b
nara <parknara0213@gmail.com>
2025-05-19 11:20:11 +0900

    Feat: [추가] SearchDropdonw - 검색어가 없는 경우의 레이아웃 - 키워드 표시는 아직 안함
    Refs: #187

--- src/features/explore/data/languages/ko_KR/index.ts
+++ src/features/explore/data/languages/ko_KR/index.ts
@@ -1,3 +1,9 @@
-const explore_kr = {};
+const explore_kr = {
+  SearchDropdown: {
+    recent: "최근",
+    saved: "저장된 검색",
+    clear: "모두 지우기",
+  },
+};
 
 export default explore_kr;
--- src/features/explore/ui/SearchDropdown/SearchDropdown.tsx
+++ src/features/explore/ui/SearchDropdown/SearchDropdown.tsx
@@ -1,6 +1,8 @@
 import styles from "./SearchDropdown.module.css";
 import { joinClassNames } from "@shared/@common/utils";
-import { IRect } from "@features/explore";
+import { IRect, useSearchContext } from "@features/explore";
+import { Button, Text } from "@shared/@common/ui/components";
+import { useLanguageContent } from "@shared/@common/models/hooks";
 
 interface SearchDropdownProps {
   className?: string;
@@ -9,10 +11,38 @@ interface SearchDropdownProps {
 
 const SearchDropdown = ({ className, rect }: SearchDropdownProps) => {
   const classNames = joinClassNames([styles["search__dropdown"], className]);
+  const { recent, saved, clear } = useLanguageContent([
+    "explore",
+    "SearchDropdown",
+  ]);
+
+  const { keyword } = useSearchContext();
 
   return (
     <div className={classNames} style={{ top: rect.top, left: 0 }}>
-      SearchDropdown
+      {/* 검색어의 유무에 따라 레이아웃이 달라짐 */}
+      {keyword ? (
+        <div className={styles["with__keyword"]}>키워드 있는 경우</div>
+      ) : (
+        <div className={styles["no__keyword"]}>
+          <div className={styles["heading"]}>
+            <Text type="heading3">{recent}</Text>
+            <Button
+              isValid
+              onClick={() => {}}
+              variant="plain"
+              fontColor="colorTheme"
+            >
+              {clear}
+            </Button>
+          </div>
+          <div className={styles["list"]}></div>
+          <div className={styles["heading"]}>
+            <Text type="heading3">{saved}</Text>
+          </div>
+          <div className={styles["list"]}></div>
+        </div>
+      )}
     </div>
   );
 };
