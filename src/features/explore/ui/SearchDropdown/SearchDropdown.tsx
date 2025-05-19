82c9fb58004dc0f315b468cc05d9a5a4a58a781a
nara <parknara0213@gmail.com>
2025-05-19 10:51:22 +0900

    Feat: [추가] SeachDropdown - 위치 지정
    Refs: #187

--- src/features/explore/ui/SearchDropdown/SearchDropdown.tsx
+++ src/features/explore/ui/SearchDropdown/SearchDropdown.tsx
@@ -1,14 +1,20 @@
 import styles from "./SearchDropdown.module.css";
 import { joinClassNames } from "@shared/@common/utils";
+import { IRect } from "@features/explore";
 
 interface SearchDropdownProps {
   className?: string;
+  rect: IRect;
 }
 
-const SearchDropdown = ({ className }: SearchDropdownProps) => {
+const SearchDropdown = ({ className, rect }: SearchDropdownProps) => {
   const classNames = joinClassNames([styles["search__dropdown"], className]);
 
-  return <div className={classNames}>SearchDropdown</div>;
+  return (
+    <div className={classNames} style={{ top: rect.top, left: 0 }}>
+      SearchDropdown
+    </div>
+  );
 };
 
 export default SearchDropdown;
