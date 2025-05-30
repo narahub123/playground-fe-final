0111558f2c7d66b89d67f23fa4de9fb59fc1115a
nara <parknara0213@gmail.com>
2025-05-31 00:13:10 +0900

    Feat:[추가] ExploreSettingModal - 체크박스 변경 및 장소 선택 탭 추가
    Refs: #187

--- src/features/explore/ui/ExploreSettingModal/ExploreSettingModal.tsx
+++ src/features/explore/ui/ExploreSettingModal/ExploreSettingModal.tsx
@@ -2,17 +2,22 @@ import { getStandAloneModal } from "@shared/@common/models/selectors";
 import styles from "./ExploreSettingModal.module.css";
 import { useLanguageContent } from "@shared/@common/models/hooks";
 import { Modal, Text } from "@shared/@common/ui/components";
-import { joinClassNames } from "@shared/@common/utils";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "@app/store";
 import { onStandAlonClose } from "@shared/@common/models/slices/modalSlice";
 import { Icon } from "@shared/@common/ui/icons";
+import { useState } from "react";
+import { joinClassNames } from "@shared/@common/utils";
+import { LuChevronRight } from "react-icons/lu";
 
-interface ExploreSettingModalProps {
-  className?: string;
+interface ExploreSettingModalProps {}
+
+interface IExploreSettings {
+  isShowingCurrentLocationContents: boolean;
+  isShowingTrend: boolean;
 }
 
-const ExploreSettingModal = ({ className }: ExploreSettingModalProps) => {
+const ExploreSettingModal = ({}: ExploreSettingModalProps) => {
   const dispatch = useAppDispatch();
   // 언어 설정
   const {
@@ -24,6 +29,11 @@ const ExploreSettingModal = ({ className }: ExploreSettingModalProps) => {
     option2,
     description2,
   } = useLanguageContent(["explore", "ExploreSettingModal"]);
+
+  const [exploreSettings, setExploreSettings] = useState<IExploreSettings>({
+    isShowingCurrentLocationContents: true,
+    isShowingTrend: true,
+  });
 
   const isOpen = useSelector(getStandAloneModal("explore"));
 
@@ -48,33 +58,107 @@ const ExploreSettingModal = ({ className }: ExploreSettingModalProps) => {
             <Text type="heading3">{title}</Text>
           </Modal.Header>
           <Modal.Body className={styles["body"]}>
-            <div style={{ padding: "0.75rem 1rem" }}>
+            <div className={styles["heading"]}>
               <Text type="heading3">{heading1}</Text>
             </div>
-            <div style={{ padding: "1rem" }}>
+            <div className={styles["wrapper"]}>
               <div
-                style={{ display: "flex", justifyContent: "space-between" }}
-                onClick={() => {}}
+                className={styles["checkbox__wrapper"]}
+                onClick={() => {
+                  setExploreSettings((prev) => {
+                    return {
+                      ...prev,
+                      isShowingCurrentLocationContents:
+                        !prev.isShowingCurrentLocationContents,
+                    };
+                  });
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    setExploreSettings((prev) => {
+                      return {
+                        ...prev,
+                        isShowingCurrentLocationContents:
+                          !prev.isShowingCurrentLocationContents,
+                      };
+                    });
+                  }
+                }}
               >
                 <Text>{option1}</Text>
-                <Icon iconName="rectCheckboxBlank" iconSize="xl" />
+                <Icon
+                  iconName={
+                    exploreSettings.isShowingCurrentLocationContents
+                      ? "rectCheckboxFill"
+                      : "rectCheckboxBlank"
+                  }
+                  className={joinClassNames([
+                    styles["checkbox__icon"],
+                    exploreSettings.isShowingCurrentLocationContents
+                      ? styles["checked"]
+                      : styles["unchecked"],
+                  ])}
+                  iconSize="2xl"
+                  tabIndex={0}
+                />
               </div>
               <Text type="expl">{description1}</Text>
             </div>
-            <div style={{ borderBottom: "1px solid #ccc" }} />
-            <div style={{ padding: "0.75rem 1rem" }}>
+            {!exploreSettings.isShowingCurrentLocationContents && (
+              <div className={styles["location"]} onClick={() => {}}>
+                <div>
+                  <Text>{"위치탐색"}</Text>
+                  <Text type="expl">{"대한민국"}</Text>
+                </div>
+                <LuChevronRight fontSize={"1.25rem"} />
+              </div>
+            )}
+            <div style={{ borderBottom: "1px solid #f1f1f1" }} />
+            <div className={styles["heading"]}>
               <Text type="heading3">{heading2}</Text>
             </div>
-            <div style={{ padding: "1rem" }}>
+            <div className={styles["wrapper"]}>
               <div
-                style={{ display: "flex", justifyContent: "space-between" }}
-                onClick={() => {}}
+                className={styles["checkbox__wrapper"]}
+                onClick={() => {
+                  setExploreSettings((prev) => {
+                    return {
+                      ...prev,
+                      isShowingTrend: !prev.isShowingTrend,
+                    };
+                  });
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    setExploreSettings((prev) => {
+                      return {
+                        ...prev,
+                        isShowingTrend: !prev.isShowingTrend,
+                      };
+                    });
+                  }
+                }}
               >
                 <Text>{option2}</Text>
-                <Icon iconName="rectCheckboxBlank" iconSize="xl" />
+                <Icon
+                  iconName={
+                    exploreSettings.isShowingTrend
+                      ? "rectCheckboxFill"
+                      : "rectCheckboxBlank"
+                  }
+                  iconSize="2xl"
+                  className={joinClassNames([
+                    styles["checkbox__icon"],
+                    exploreSettings.isShowingTrend
+                      ? styles["checked"]
+                      : styles["unchecked"],
+                  ])}
+                  tabIndex={0}
+                />
               </div>
               <Text type="expl">{description2}</Text>
             </div>
+            <div className={styles["empty"]} />
           </Modal.Body>
         </Modal.Content>
       </Modal.Container>
