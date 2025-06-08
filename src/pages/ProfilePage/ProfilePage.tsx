7ab1c906b8292063d57dbedd1a2393e555ceea9f
nara <parknara0213@gmail.com>
2025-06-09 04:25:20 +0900

    Feat: [추가] ProfilePage - 프로필 레이아웃 추가
    Refs: #184

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,21 @@
 import styles from "./ProfilePage.module.css";
-import { selectIsFeedLoading } from "@shared/@common/models/selectors";
-import { Spinner } from "@shared/@common/ui/components";
+import {
+  selectIsFeedLoading,
+  selectUser,
+} from "@shared/@common/models/selectors";
+import {
+  Button,
+  ProfileImage,
+  Spinner,
+  Text,
+} from "@shared/@common/ui/components";
 import { joinClassNames } from "@shared/@common/utils";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { ProfilePageTab, proflieTabLinks } from "@features/profile-page";
+import { Icon } from "@shared/@common/ui/icons";
+import { defaultProfileImage } from "@shared/@common/assets";
+import { Link } from "react-router-dom";
 
 interface ProfilePageProps {
   className?: string;
@@ -12,11 +23,88 @@ interface ProfilePageProps {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
   const isLoading = useSelector(selectIsFeedLoading);
+  const user = useSelector(selectUser);
 
   const classNames = joinClassNames([styles["profile__page"], className]);
 
   return (
     <div className={classNames}>
+      <div className={styles["header"]}>
+        <div className={styles["icon__wrapper"]}>
+          <Icon
+            iconName="arrowLeft"
+            className={styles["icon"]}
+            onClick={() => {}}
+          />
+        </div>
+        <div className={styles["username__wrapper"]}>
+          <Text type="heading3" status="bold">
+            {user.username}
+          </Text>
+          <Text type="expl">{"게시물"}</Text>
+        </div>
+      </div>
+      <div className={styles["cover__wrapper"]}>
+        {user.profileCoverImage && (
+          <img src={user.profileCoverImage} alt="커버" />
+        )}
+      </div>
+      <div className={styles["profile__wrapper"]}>
+        <div className={styles["button__wrapper"]}>
+          <div className={styles["profile__image__container"]}>
+            <div className={styles["profile__image__wrapper"]}>
+              <ProfileImage
+                rounded="full"
+                src={user.profileImage || defaultProfileImage}
+              />
+            </div>
+          </div>
+          <Button
+            variant="outline"
+            isValid
+            onClick={() => {}}
+            rounded="2xl"
+            className={styles["button"]}
+          >
+            {"프로필 수정"}
+          </Button>
+        </div>
+        <div className={styles["userinfo__container"]}>
+          <div className={styles["userinfo__wrapper"]}>
+            <Text type="heading3">{user.username}</Text>
+            <Text type="expl">{`@${user.userId}`}</Text>
+          </div>
+          <div className={styles["authorize__wrapper"]}>
+            <Button
+              variant="outline"
+              isValid
+              onClick={() => {}}
+              className={styles["authorize__button"]}
+              rounded="2xl"
+              fontSize="sm"
+            >
+              {"인증받기"}
+            </Button>
+          </div>
+        </div>
+        <div className={styles["regiDate__wrapper"]}>
+          <Text>{`가입일: ${user.createdAt}`}</Text>
+        </div>
+        <div className={styles["meta__wrapper"]}>
+          <Link
+            className={styles["meta__unit"]}
+            to=""
+            style={{ marginRight: "20px" }}
+          >
+            <strong>{user.followings.length}</strong>
+            <Text>{"팔로우 중"}</Text>
+          </Link>
+          <Link className={styles["meta__unit"]} to="">
+            <strong>{user.followers.length}</strong>
+            <Text>{"팔로워"}</Text>
+          </Link>
+        </div>
+      </div>
       <div className={styles["tabs"]}>
         {Object.keys(proflieTabLinks).map((key: string) => (
           <ProfilePageTab key={key} link={key} />
